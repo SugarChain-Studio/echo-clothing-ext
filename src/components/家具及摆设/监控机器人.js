30912905645648476ccba16889c015aec045fe35
dynilath <dynilath@qq.com>
2025-02-23 09:02:50 +0800

    feat: 监控机器人眼睛动作

--- src/components/家具及摆设/监控机器人.js
+++ src/components/家具及摆设/监控机器人.js
@@ -22,6 +22,7 @@ const asset = {
         { Name: "Soft", Group: "Eyebrows", Timer: 5 },
     ],
     DynamicBeforeDraw: true,
+    DynamicScriptDraw: true,
     DefaultColor: ["#84DBFF", "#B2E8FF"],
     FixedPosition: true,
     Layer: [
@@ -48,6 +49,14 @@ const asset = {
                 KneelingSpread: "Kneel",
                 Suspension: PoseType.HIDE,
             },
+        },
+        {
+            Name: "眼背景",
+            AllowColorize: false,
+        },
+        {
+            Name: "眼睛",
+            HasImage: false,
         },
         {
             Name: "机器人",
@@ -115,11 +124,19 @@ const dialog = Tools.replicateTypedItemDialog(["ItemNeckRestraints"], ["监控�
     },
 });
 
+/**
+ * @typedef { {X:number,Y:number}} Position
+ * @typedef { { EyeTimer:number, TargetOffset: Position, CurOffset:Position, UpdateTimer:number, FrameTimer:number }} DataType
+ */
+
 export default function () {
     ModManager.globalFunction(
         `AssetsItemNeckRestraints${asset.Name}BeforeDraw`,
-        /** @type {ExtendedItemCallbacks.BeforeDraw} */ (
-            ({ C, L, Property }) => {
+        /** @type {ExtendedItemCallbacks.BeforeDraw<DataType>} */ (
+            (drawData) => {
+                const { C, A, L, X, Y, Property, PersistentData, drawCanvas, drawCanvasBlink } = drawData;
+                const Data = PersistentData();
+
                 if (Property?.TypeRecord?.typed === 1) {
                     if (L === "跟随模式" && C.HasEffect("IsLeashed")) {
                         return { Opacity: 0 };
@@ -128,6 +145,67 @@ export default function () {
                         return { Opacity: 0 };
                     }
                 }
+
+                const next = (now) => now + (Math.random() * 10 + 2) * 1000;
+
+                if (L === "眼睛")
+                    (() => {
+                        const now = Date.now();
+                        if (!Data.EyeTimer) Data.EyeTimer = next(now);
+                        if (!Data.TargetOffset) Data.TargetOffset = { X: 0, Y: 0 };
+                        if (!Data.CurOffset) Data.CurOffset = { X: 0, Y: 0 };
+                        if (!Data.UpdateTimer) {
+                            Data.UpdateTimer = now;
+                            return;
+                        }
+
+                        const delta = now - Data.UpdateTimer;
+                        Data.UpdateTimer = now;
+
+                        if (now > Data.EyeTimer) {
+                            Data.EyeTimer = next(now);
+                            const randX = Math.random();
+                            const randY = Math.random();
+                            Data.TargetOffset = {
+                                X: (randX * randX - 0.5) * 8,
+                                Y: (randY * randY - 0.5) * 8,
+                            };
+                        }
+
+                        const canvas = AnimationGenerateTempCanvas(C, A, 150, 230);
+                        const layerSrc = Tools.getAssetURL(drawData);
+
+                        const dx = Data.TargetOffset.X - Data.CurOffset.X;
+                        const dy = Data.TargetOffset.Y - Data.CurOffset.Y;
+
+                        const hy = Math.hypot(dx, dy);
+
+                        if (hy < 0.01) {
+                            Data.CurOffset = Data.TargetOffset;
+                        } else {
+                            // speed = 6;
+                            const caphy = Math.min(hy, (6 * delta) / 1000);
+                            const mx = (dx / hy) * caphy;
+                            const my = (dy / hy) * caphy;
+                            Data.CurOffset.X += mx;
+                            Data.CurOffset.Y += my;
+                        }
+
+                        DrawImageEx(layerSrc, canvas.getContext("2d"), Data.CurOffset.X, Data.CurOffset.Y);
+
+                        drawCanvas(canvas, X, Y);
+                        drawCanvasBlink(canvas, X, Y);
+                    })();
+            }
+        )
+    );
+
+    ModManager.globalFunction(
+        `AssetsItemNeckRestraints${asset.Name}ScriptDraw`,
+        /** @type {ExtendedItemCallbacks.ScriptDraw<DataType>} */ (
+            ({ C, Item, PersistentData }) => {
+                const Data = PersistentData();
+                Tools.drawUpdate(C, Data);
             }
         )
     );
