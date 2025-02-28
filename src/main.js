aadc95181213808b4ed88d9bad60ee3c927b8c70
dynilath <dynilath@qq.com>
2025-03-01 06:29:27 +0800

    feat: 在登录后加载时，在控制台提示加载可能不正确

--- src/main.js
+++ src/main.js
@@ -4,8 +4,19 @@ import { ModInfo } from "@mod-utils/rollupHelper";
 
 import { setup } from "./components";
 import { once } from "@mod-utils/loadFlag";
+import log from "@mod-utils/log";
+
+const message = {
+    en: "Player appearance is already loaded when enabling custom assets, some assets may be lost.",
+    zh: "启用自定义资源时，玩家外观已经加载，可能会丢失一些资源。",
+};
 
 once(ModInfo.name, () => {
     ModManager.init(ModInfo);
     AssetManager.init(setup);
+
+    if (Player?.MemberNumber) {
+        const userLanguage = navigator.language.startsWith("zh") ? "zh" : "en";
+        log.warn(message[userLanguage]);
+    }
 });
