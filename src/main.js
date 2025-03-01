359650a4547e6c0db3ab2f2312ca50578828a096
dynilath <dynilath@qq.com>
2025-03-01 08:48:33 +0800

    chore: 更新子项目，让部分log支持多语言

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import { once } from "@mod-utils/loadFlag";
 import log from "@mod-utils/log";
 
 const message = {
-    en: "Player appearance is already loaded when enabling custom assets, some assets may be lost.",
-    zh: "启用自定义资源时，玩家外观已经加载，可能会丢失一些资源。",
+    en: "Initiating custom assets registration after player appearance loaded, some assets may be lost.",
+    zh: "在玩家外观加载后初始化自定义资产注册，部分资产可能丢失。",
 };
 
 once(ModInfo.name, () => {
