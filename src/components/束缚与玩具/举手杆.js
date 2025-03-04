5c3b7db8060c5e701300ebb8232cc7204a2cab86
XinLian132243 <XinLian132243@protonmail.com>
2025-03-05 01:26:50 +0800

    feat: 增加举手杆和分膝杆

---
+++ src/components/束缚与玩具/举手杆.js
@@ -0,0 +1,35 @@
+import AssetManager from "@mod-utils/AssetManager";
+import { VersionSupport } from "@mod-utils/VersionSupport";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "举手杆",
+    Left: 0,
+    Top: 0,
+    Difficulty: 5,
+    Priority: 39,
+    Time: 12,
+    RemoveTime: 10,
+    Extended: false,
+    AllowLock: true,
+    DrawLocks: true,
+    AllowTighten: true,
+    Effect: [E.Block, E.BlockWardrobe],
+    AllowActivePose: ["OverTheHead"],
+    SetPose: ["OverTheHead"],
+    ParentGroup: VersionSupport.NoParentGroup,  
+    Layer: [
+        { Name: "杆子"},
+        { Name: "颈部"},
+        { Name: "束带"},
+    ],
+};
+
+const translations = {
+    CN: "举手杆",
+    EN: "Hand raised bar"
+};
+
+export default function () {
+    AssetManager.addAsset("ItemArms", asset, null, translations);
+}
---
+++ src/components/束缚与玩具/分膝杆.js
@@ -0,0 +1,34 @@
+import AssetManager from "@mod-utils/AssetManager";
+import { VersionSupport } from "@mod-utils/VersionSupport";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "分膝杆",
+    Left: 59,
+    Top: 0,
+    Difficulty: 5,
+    Priority: 39,
+    Time: 12,
+    RemoveTime: 10,
+    Extended: false,
+    AllowLock: true,
+    DrawLocks: true,
+    AllowTighten: true,
+    Effect: [E.Freeze, E.BlockWardrobe],
+    AllowActivePose: ["KneelingSpread"],
+    SetPose: ["KneelingSpread"],
+    ParentGroup: VersionSupport.NoParentGroup, 
+    Layer: [
+        { Name: "杆子"},
+        { Name: "束带", ParentGroup: "BodyLower"},
+    ],
+};
+
+const translations = {
+    CN: "分膝杆",
+    EN: "Split knee bar"
+};
+
+export default function () {
+    AssetManager.addAsset("ItemLegs", asset, null, translations);
+}
