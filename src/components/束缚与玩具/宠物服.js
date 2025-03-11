5d8d82fe091e7f9a86924adacdd0a083cfb4fe93
emdsa2 <1260556316@qq.com>
2025-03-12 07:24:41 +0800

    feat: 宠物服上添加锁链

--- src/components/束缚与玩具/宠物服.js
+++ src/components/束缚与玩具/宠物服.js
@@ -41,7 +41,8 @@ const asset = {
         AllFours: "AllFours",
     },
     Layer: [
-        { Name: "手臂",
+        {
+            Name: "手臂",
             Priority: 9,
             Alpha: [
                 {
@@ -83,11 +84,25 @@ const asset = {
                     Pose: ["Yoked"],
                 },
             ],
-         },
+        },
         { Name: "拘束" },
         { Name: "拘束高光" },
         { Name: "束带" },
         { Name: "搭扣" },
+        {
+            Name: "手链链子",
+            Priority: 8,
+            AllowTypes: { l: 1 },
+            Top: -430,
+            Left: 0,
+        },
+        {
+            Name: "脚链链子",
+            Priority: 8,
+            AllowTypes: { ll: 1 },
+            Top: 530,
+            Left: 0,
+        },
     ],
 };
 
@@ -125,6 +140,125 @@ const asset2 = {
     ],
 };
 
+/**@type {AssetArchetypeConfig} */
+const extened = {
+    Archetype: ExtendedArchetype.MODULAR,
+    ChangeWhenLocked: false,
+    Modules: [
+        {
+            Name: "锁链手",
+            DrawImages: false,
+            Key: "l",
+            Options: [
+                {
+                    Property: { 
+                        Difficulty: 8, 
+                        Effect: [E.Block, E.BlockWardrobe],
+                        AllowActivePose: ["BackElbowTouch", "OverTheHead", "Yoked", "AllFours"],
+                        SetPose: ["BackElbowTouch"] },
+                    Expression: [{ Group: "Blush", Name: "Medium", Timer: 10 }],
+                },
+                {
+                    Property: { 
+                        Difficulty: 8, 
+                        Effect: [E.CuffedArms, E.Mounted], 
+                        SetPose: ["OverTheHead"] },
+                    Expression: [{ Group: "Blush", Name: "Medium", Timer: 10 }],
+                },
+            ],
+        },    
+        {
+            Name: "锁链腿",
+            DrawImages: false,
+            Key: "ll",
+            Options: [
+                {},
+                {
+                    Property: {
+                        Difficulty: 8,
+                        Effect: [E.CuffedFeet, E.Mounted],
+                        SetPose: ["KneelingSpread"],
+                    },
+                    Expression: [{ Group: "Blush", Name: "Medium", Timer: 10 }],
+                },
+            ],
+        },
+        {
+            Name: "自定义高度",
+            DrawImages: false,
+            Key: "lll",
+            Options: [
+                {},
+                {
+                    HasSubscreen: true,
+                    ArchetypeConfig: {
+                        Archetype: ExtendedArchetype.VARIABLEHEIGHT,
+                        MaxHeight: 0,
+                        MinHeight: -250,
+                        DrawData: {
+                            elementData: [{ position: [1140, 650, 100, 500], icon: "rope" }],
+                        },
+                        DialogPrefix: {
+                            Chat: "SuspensionChange",
+                        },
+                    },
+                },
+            ],
+        },
+    ],
+};
+
+/**@type {Translation.Dialog} */
+const dialog = {
+    CN: {
+        ItemArms宠物服上SelectBase: "添加锁链",
+        ItemArms宠物服上Select锁链手: "添加锁链",
+        ItemArms宠物服上Module锁链手: "手部铐子",
+        ItemArms宠物服上Optionl0: "无",
+        ItemArms宠物服上Optionl1: "添加锁链",
+        ItemArms宠物服上Setl0: "SourceCharacter移除了DestinationCharacter身上的锁链",
+        ItemArms宠物服上Setl1: "SourceCharacter在DestinationCharacter手臂加上了锁链",
+
+        ItemArms宠物服上Select锁链腿: "添加锁链",
+        ItemArms宠物服上Module锁链腿: "腿部铐子",
+        ItemArms宠物服上Optionll0: "无",
+        ItemArms宠物服上Optionll1: "添加锁链",
+        ItemArms宠物服上Setll0: "SourceCharacter移除了DestinationCharacter身上的锁链",
+        ItemArms宠物服上Setll1: "SourceCharacter在DestinationCharacter腿上加上了锁链",
+
+        ItemArms宠物服上Select自定义高度: "设置高度",
+        ItemArms宠物服上Module自定义高度: "调整高度",
+        ItemArms宠物服上Optionlll0: "无",
+        ItemArms宠物服上Optionlll1: "自定义高度",
+        ItemArms宠物服上Setlll0: "SourceCharacter还原DestinationCharacter高度",
+        ItemArms宠物服上Setlll1: "SourceCharacter调整DestinationCharacter高度",
+
+    },
+EN: {
+    ItemArms宠物服上SelectBase: "Add Chains",
+    ItemArms宠物服上Select锁链手: "Add Hand Chains",
+    ItemArms宠物服上Module锁链手: "Hand Cuffs",
+    ItemArms宠物服上Optionl0: "None",
+    ItemArms宠物服上Optionl1: "Add Chains",
+    ItemArms宠物服上Setl0: "SourceCharacter removed chains from DestinationCharacter's arms",
+    ItemArms宠物服上Setl1: "SourceCharacter added chains to DestinationCharacter's arms",
+
+    ItemArms宠物服上Select锁链腿: "Add Leg Chains",
+    ItemArms宠物服上Module锁链腿: "Leg Cuffs",
+    ItemArms宠物服上Optionll0: "None",
+    ItemArms宠物服上Optionll1: "Add Chains",
+    ItemArms宠物服上Setll0: "SourceCharacter removed chains from DestinationCharacter's legs",
+    ItemArms宠物服上Setll1: "SourceCharacter added chains to DestinationCharacter's legs",
+
+    ItemArms宠物服上Select自定义高度: "Set Height",
+    ItemArms宠物服上Module自定义高度: "Adjust Height",
+    ItemArms宠物服上Optionlll0: "None",
+    ItemArms宠物服上Optionlll1: "Custom Height",
+    ItemArms宠物服上Setlll0: "SourceCharacter restored DestinationCharacter's height",
+    ItemArms宠物服上Setlll1: "SourceCharacter adjusted DestinationCharacter's height",
+},
+};
+
 const translations = {
     CN: "宠物服上",
     EN: "Pet Upper Suit",
@@ -140,6 +274,7 @@ const translations2 = {
 };
 
 export default function () {
-    AssetManager.addAsset("ItemArms", asset, undefined, translations);
+    AssetManager.addAsset("ItemArms", asset, extened, translations);
     AssetManager.addAsset("ItemLegs", asset2, undefined, translations2);
+    AssetManager.addCustomDialog(dialog);
 }
