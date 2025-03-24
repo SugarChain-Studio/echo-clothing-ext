22a1f3791a3ad2d78dca9e81f92184d18e09a4eb
dynilath <dynilath@qq.com>
2025-03-25 03:17:44 +0800

    chore: 添加调试工具，隐藏身体道具和截图隐藏背景

---
+++ src/components/0模板/调试.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-unused-vars */
+import { AssetManager } from "../../assetForward";
+import { HookManager } from "@sugarch/bc-mod-hook-manager";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "隐藏身体",
+    Hide: [
+        "BodyLower",
+        "BodyUpper",
+        "HairFront",
+        "HairBack",
+        "Eyes",
+        "Eyes2",
+        "Mouth",
+        "Nipples",
+        "Pussy",
+        "Head",
+        "ArmsRight",
+        "ArmsLeft",
+        "HandsLeft",
+        "HandsRight",
+        "Blush",
+        "EyeShadow",
+        "Eyebrows",
+    ],
+};
+
+function load() {
+    // 一个能隐藏身体的道具
+    AssetManager.addAsset("BodyMarkings2_Luzi", asset);
+
+    // 聊天室隐藏图标设置为闭眼的时候，截角色图时隐藏背景
+    HookManager.patchFunction("DialogDraw", {
+        "if (!CurrentCharacter) return;":
+            "if(ChatRoomHideIconState >= 2) MainCanvas.clearRect(0, 0, 2000, 1000); if (!CurrentCharacter) return;",
+    });
+}
+
+// 取消注释以启用
+// export default load;
