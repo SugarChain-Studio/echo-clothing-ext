250e71fe210e76732c4659d48c36a03945c00f94
dynilath <dynilath@qq.com>
2025-03-25 02:45:19 +0800

    feat: 添加铁架台及其相关资源和配置

---
+++ src/components/家具及摆设/铁架台.js
@@ -0,0 +1,190 @@
+import { AssetManager } from "../../assetForward";
+import { DialogTools, Tools } from "@mod-utils/Tools";
+import { VersionSupport } from "@mod-utils/VersionSupport";
+
+/** @type {TopLeft.Definition} */
+const baseTop = {
+    [PoseType.DEFAULT]: 800,
+    Kneel: 550,
+    KneelingSpread: 550,
+};
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "铁架台",
+    ParentGroup: VersionSupport.NoParentGroup,
+    Random: false,
+    Left: 0,
+    Top: 0,
+    DefaultColor: [],
+    PoseMapping: {},
+    AllowLock: true,
+    Effect: [E.Mounted, E.BlockWardrobe],
+    Difficulty: 8,
+    Layer: [
+        {
+            Top: baseTop,
+            Name: "底座",
+            Priority: 5,
+        },
+        {
+            Top: baseTop,
+            Name: "阴影",
+            AllowColorize: false,
+            Priority: 5,
+        },
+        {
+            Top: baseTop,
+            Name: "螺丝",
+            Priority: 5,
+        },
+        {
+            Top: {
+                [PoseType.DEFAULT]: 0,
+                Kneel: -250,
+                KneelingSpread: -250,
+            },
+            Left: 300,
+            Name: "竖杆",
+            Priority: 5,
+        },
+        {
+            Top: 140,
+            Name: "横杆",
+            Priority: 5,
+        },
+        {
+            Top: 140,
+            Name: "横杆扣件",
+            Priority: 5,
+        },
+        {
+            Top: 140,
+            Name: "横枷铐阴影",
+            AllowColorize: false,
+            Priority: 5,
+            AllowTypes: { y: 1 },
+        },
+        {
+            Top: 140,
+            Name: "横枷铐",
+            Priority: 5,
+            AllowTypes: { y: 1 },
+        },
+        {
+            Top: 140,
+            Name: "横枷铐前",
+            Priority: 34,
+            AllowTypes: { y: 1 },
+        },
+        {
+            Top: 140,
+            Name: "项圈阴影",
+            AllowColorize: false,
+            Priority: 5,
+        },
+        {
+            Top: 140,
+            Name: "项圈",
+            Priority: 5,
+        },
+        {
+            Top: 140,
+            Name: "项圈前",
+            Priority: 34,
+        },
+    ],
+};
+
+/** @type {ModularItemConfig} */
+const extended = {
+    Archetype: ExtendedArchetype.MODULAR,
+    ChatTags: Tools.CommonChatTags(),
+    Modules: [
+        {
+            Name: "高度",
+            Key: "h",
+            DrawImages: true,
+            Options: [
+                {
+                    Property: {
+                        SetPose: ["LegsClosed"],
+                        AllowActivePose: [...PoseAllStanding],
+                    },
+                },
+                {
+                    Property: {
+                        SetPose: ["Kneel"],
+                        AllowActivePose: [...PoseAllKneeling],
+                    },
+                },
+            ],
+        },
+        {
+            Name: "横枷",
+            Key: "y",
+            DrawImages: true,
+            Options: [
+                {},
+                {
+                    Difficulty: 16,
+                    Property: {
+                        Effect: [E.Block, E.NotSelfPickable],
+                        SetPose: ["Yoked"],
+                    },
+                },
+            ],
+        },
+    ],
+};
+
+/** @type {Translation.Entry} */
+const translations = {
+    CN: "铁架台",
+    EN: "Buret Stand",
+};
+
+/** @type {Translation.Dialog} */
+const dialog = DialogTools.replicateGroupedItemDialog(["ItemDevices"], [asset.Name], {
+    CN: {
+        SelectBase: "选择铁架台设置",
+        Module高度: "高度",
+        Module横枷: "横枷",
+
+        Select高度: "选择铁架台高度",
+        Optionh0: "站立",
+        Optionh1: "跪姿",
+
+        Seth0: "SourceCharacter将DestinationCharacterAssetName的横杆抬升到勉强踮脚站立的高度。",
+        Seth1: "SourceCharacter将DestinationCharacterAssetName的横杆降低到不得不跪下的高度。",
+
+        Select横枷: "选择铁架台横枷",
+        Optiony0: "无",
+        Optiony1: "有",
+        Sety0: "SourceCharacter解开DestinationCharacterAssetName上的横枷，解放了DestinationCharacter双手。",
+        Sety1: "SourceCharacter将DestinationCharacterAssetName的横枷锁上，让DestinationCharacter双手失去自由。",
+    },
+    EN: {
+        SelectBase: "Select Buret Stand Configuration",
+        Module高度: "Height",
+        Module横枷: "Yoke",
+
+        Select高度: "Select Buret Stand Height",
+        Optionh0: "Standing",
+        Optionh1: "Kneeling",
+
+        Seth0: "SourceCharacter raises the horizontal bar of DestinationCharacter AssetName to a height where TargetCharacter can barely stand on her toes.",
+        Seth1: "SourceCharacter lowers the horizontal bar of DestinationCharacter AssetName to a height where TargetCharacter has to kneel down.",
+
+        Select横枷: "Select Buret Stand Yoke",
+        Optiony0: "None",
+        Optiony1: "Yoked",
+        Sety0: "SourceCharacter unlocks the yoke on DestinationCharacter AssetName, freeing DestinationCharacter hands.",
+        Sety1: "SourceCharacter locks the yoke on DestinationCharacter AssetName, depriving DestinationCharacter hands of freedom.",
+    },
+});
+
+export default function () {
+    AssetManager.addAsset("ItemDevices", asset, extended, translations);
+    AssetManager.addCustomDialog(dialog);
+}
