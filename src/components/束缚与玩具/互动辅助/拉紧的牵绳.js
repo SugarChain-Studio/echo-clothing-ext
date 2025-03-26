a8342128ec836e3c6de3ebce9f2e04fbbaeeb4b8
dynilath <dynilath@qq.com>
2025-03-26 12:23:29 +0800

    refactor: 增加一种拉紧的牵绳资产

--- src/components/束缚与玩具/互动辅助/拉紧的牵绳.js
+++ src/components/束缚与玩具/互动辅助/拉紧的牵绳.js
@@ -1,26 +1,28 @@
 import { AssetManager } from "../../../assetForward";
 import { DrawCharacterModifier, ChatRoomOrder } from "@mod-utils/ChatRoomOrder";
 
+const asset = {
+    Name: "拉紧的牵绳_Luzi",
+    Random: false,
+    Value: -1,
+    Visible: false,
+    NotVisibleOnScreen: ["LuziScreen"], // 使用这个数据来让物品在列表不显示
+};
+
+const translations = {
+    CN: "拉紧的牵绳",
+    EN: "Pulled Leash",
+    RU: "Потянутый повод",
+};
+
 export default function () {
-    AssetManager.addAsset(
-        "ItemHandheld",
-        {
-            Name: "拉紧的牵绳_Luzi",
-            Random: false,
-            Value: -1,
-            Visible: false,
-            NotVisibleOnScreen: ["LuziScreen"], // 使用这个数据来让物品在列表不显示
-        },
-        undefined,
-        {
-            CN: "拉紧的牵绳",
-            EN: "Pulled Leash",
-            RU: "Потянутый повод",
-        }
-    );
+    AssetManager.addAsset("ItemHandheld", asset, undefined, translations);
+    AssetManager.addAsset("ItemHandheld", { ...asset, Name: "拉紧的链子_Luzi" }, undefined, translations);
 
     AssetManager.addImageMapping({
         "Assets/Female3DCG/ItemHandheld/Preview/拉紧的牵绳_Luzi.png":
+            "Assets/Female3DCG/ItemNeckRestraints/Preview/CollarLeash.png",
+        "Assets/Female3DCG/ItemHandheld/Preview/拉紧的链子_Luzi.png":
             "Assets/Female3DCG/ItemNeckRestraints/Preview/CollarLeash.png",
     });
 
@@ -29,10 +31,9 @@ export default function () {
         const sharedC = ChatRoomOrder.requireSharedCenter(C);
         if (!sharedC) return arg;
 
-        if (
-            sharedC.prev.XCharacterDrawOrder.associatedAsset?.asset !== "CollarLeash" ||
-            sharedC.next.XCharacterDrawOrder.associatedAsset?.asset !== "拉紧的牵绳_Luzi"
-        )
+        const assetName = sharedC.prev.XCharacterDrawOrder.associatedAsset?.asset;
+
+        if (assetName !== "CollarLeash" && assetName !== "拉紧的牵绳_Luzi" && assetName !== "拉紧的链子_Luzi")
             return arg;
 
         if (sharedC.prev.MemberNumber === C.MemberNumber) {
