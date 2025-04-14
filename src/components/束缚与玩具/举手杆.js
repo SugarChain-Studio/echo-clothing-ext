68d74e5ad551480f139bf66077ae41f63ea65dc5
dynilath <dynilath@qq.com>
2025-04-15 01:12:15 +0800

    refactor: 更新举手杆资源，允许锁图层调色，补充图层翻译

--- src/components/束缚与玩具/举手杆.js
+++ src/components/束缚与玩具/举手杆.js
@@ -11,20 +11,35 @@ const asset = {
     RemoveTime: 10,
     Extended: false,
     AllowLock: true,
-    DrawLocks: true,
     AllowTighten: true,
+    Random: false,
     Effect: [E.Block, E.BlockWardrobe],
     AllowActivePose: ["OverTheHead"],
     SetPose: ["OverTheHead"],
     ParentGroup: {},
-    Layer: [{ Name: "杆子" }, { Name: "颈部" }, { Name: "束带" }],
+    Layer: [{ Name: "杆子" }, { Name: "颈部" }, { Name: "束带" }, { Name: "Lock", LockLayer: true }],
 };
 
-const translations = {
+const description = {
     CN: "举手杆",
     EN: "Hand raised bar",
 };
 
+const layerNames = {
+    CN: {
+        杆子: "杆子",
+        颈部: "颈部",
+        束带: "束带",
+        Lock: "锁",
+    },
+    EN: {
+        杆子: "Bar",
+        颈部: "Neck",
+        束带: "Belt",
+        Lock: "Lock",
+    },
+};
+
 export default function () {
-    AssetManager.addAsset("ItemArms", asset, null, translations);
+    AssetManager.addAssetWithConfig("ItemArms", asset, { description, layerNames });
 }
