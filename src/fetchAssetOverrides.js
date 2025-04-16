6132e0d6f216d722a3b259ad0ea71db591036cf6
dynilath <dynilath@qq.com>
2025-04-17 01:00:07 +0800

    refactor: debug时保证资源更新

--- src/fetchAssetOverrides.js
+++ src/fetchAssetOverrides.js
@@ -1,11 +1,15 @@
-import { assetOverridesURL, resourceBaseURL, ModInfo } from "@mod-utils/rollupHelper";
+import { assetOverridesURL, resourceBaseURL, ModInfo, debugFlag } from "@mod-utils/rollupHelper";
 import { AssetManager, resolveAssetOverrides } from "@sugarch/bc-asset-manager";
 
 async function fetchWithRetry(url, retries = 3, delay = 1000) {
     for (let attempt = 1; attempt <= retries; attempt++) {
         try {
             const urlWithVersion = new URL(url);
-            urlWithVersion.searchParams.set("version", ModInfo.version);
+            if (debugFlag) {
+                urlWithVersion.searchParams.set("version", `${Date.now()}`);
+            } else {
+                urlWithVersion.searchParams.set("version", ModInfo.version);
+            }
 
             const response = await fetch(urlWithVersion.toString());
             if (!response.ok) {
