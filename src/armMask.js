7fe95cf1d8529796016268e899dca99cde65754b
dynilath <dynilath@qq.com>
2025-04-19 04:01:52 +0800

    refactor: 修正手臂遮罩背后姿势，增加一种手臂遮罩实现，提升版本简化类型处理

--- src/armMask.js
+++ src/armMask.js
@@ -3,64 +3,100 @@ import { AssetManager } from "./assetForward";
 const bodySizes = ["Small", "Normal", "Large", "XLarge", "FlatSmall", "FlatMedium"];
 
 /**
+ * @typedef {"" | "Arm1"} ArmMaskMode
+ */
+
+/** @type {Record<ArmMaskMode,string>} */
+const nameRecord = {
+    "": "ArmMask",
+    "Arm1": "ArmMask1",
+};
+
+/**
  * @param {CustomGroupName | CustomGroupName []} groupName
  * @param {string} assetName
+ * @param { ArmMaskMode } mode
  */
-function createMappings(groupName, assetName) {
+function createMappings(groupName, assetName, mode) {
     /** @type {Record<string,string>} */
     const ret = {};
     const groupNames = Array.isArray(groupName) ? groupName : [groupName];
+
+    const maskName = nameRecord[mode];
 
     for (const group of groupNames) {
         for (const size of bodySizes) {
             // Assets/Female3DCG/Cloth/绛云墨韵旗袍裙_Luzi_Normal_ArmMask.png
             ret[
-                `Assets/Female3DCG/${group}/${assetName}_${size}_ArmMask.png`
-            ] = `Assets/Female3DCG/LuziArmMask/ArmMask_${size}.png`;
+                `Assets/Female3DCG/${group}/${assetName}_${size}_${maskName}.png`
+            ] = `Assets/Female3DCG/LuziArmMask/${maskName}_${size}.png`;
             ret[
-                `Assets/Female3DCG/${group}/TapedHands/${assetName}_${size}_ArmMask.png`
-            ] = `Assets/Female3DCG/LuziArmMask/TapedHands/ArmMask_${size}.png`;
+                `Assets/Female3DCG/${group}/TapedHands/${assetName}_${size}_${maskName}.png`
+            ] = `Assets/Female3DCG/LuziArmMask/TapedHands/${maskName}_${size}.png`;
         }
     }
 
     return ret;
 }
 
-const layerDef = /** @type {any}*/ ({
-    Name: "ArmMask",
-    ParentGroup: "BodyUpper",
-    PoseMapping: {
-        BackBoxTie: PoseType.HIDE,
-        BackCuffs: PoseType.HIDE,
-        Yoked: PoseType.HIDE,
-        OverTheHead: PoseType.HIDE,
-        Hogtied: PoseType.HIDE,
-        TapedHands: "TapedHands",
-    },
-    TextureMask: {},
-    BlendingMode: "destination-out",
-});
-
 /**
- * 为衣物创建手臂遮罩
- * @param {CustomGroupName | CustomGroupName []} groupName
- * @param {CustomAssetDefinition} asset
+ * @param { ArmMaskMode } mode
+ * @returns {AssetLayerDefinition}
  */
-function createArmMaskForCloth(groupName, asset) {
-    if (GameVersion === "R114") return;
-    asset.Layer?.push(layerDef) ?? (asset.Layer = [layerDef]);
-    AssetManager.addImageMapping(createMappings(groupName, asset.Name));
+function createLayerDef(mode) {
+    return {
+        Name: nameRecord[mode],
+        ParentGroup: "BodyUpper",
+        PoseMapping: {
+            BackBoxTie: PoseType.HIDE,
+            BackCuffs: PoseType.HIDE,
+            BackElbowTouch: PoseType.HIDE,
+            Yoked: PoseType.HIDE,
+            OverTheHead: PoseType.HIDE,
+            Hogtied: PoseType.HIDE,
+            TapedHands: "TapedHands",
+        },
+        TextureMask: {},
+        BlendingMode: "destination-out",
+    };
 }
 
 /**
- * 为分组衣物创建手臂遮罩
- * @param {CustomGroupedAssetDefinitions} assets
+ * 为衣物创建手臂遮罩。
+ *
+ * 默认遮罩到Top=367；`mode = "Arm1"` 时，遮罩扩展到Top=353
+ *
+ * @param {CustomGroupName | CustomGroupName []} groupName
+ * @param {CustomAssetDefinition} asset
+ * @param {ArmMaskMode} mode
  */
-function createArmMaskForGroupedCloth(assets) {
+function createArmMaskForCloth(groupName, asset, mode = "") {
     if (GameVersion === "R114") return;
+    const layerDef = createLayerDef(mode);
+    asset.Layer?.push(layerDef) ?? (asset.Layer = [layerDef]);
+    AssetManager.addImageMapping(createMappings(groupName, asset.Name, mode));
+}
+
+/**
+ * @typedef {Partial<Record<CustomGroupName, Record<string,ArmMaskMode>>>} GroupedAssetMaskMode
+ */
+
+/**
+ * 为分组衣物创建手臂遮罩。
+ *
+ * 默认遮罩到Top=367；`mode = "Arm1"` 时，遮罩扩展到Top=353
+ *
+ * @param {CustomGroupedAssetDefinitions} assets
+ * @param {ArmMaskMode | GroupedAssetMaskMode} mode
+ */
+function createArmMaskForGroupedCloth(assets, mode = "") {
+    if (GameVersion === "R114") return;
+
+    const resolveMode = typeof mode === "string" ? (_1, _2) => mode : (group, asset) => mode[group][asset.Name] ?? "";
+
     for (const [group, assetArr] of Object.entries(assets)) {
         for (const asset of assetArr) {
-            createArmMaskForCloth(/**@type {CustomGroupName}*/ (group), asset);
+            createArmMaskForCloth(/**@type {CustomGroupName}*/ (group), asset, resolveMode(group, asset));
         }
     }
 }
