b2dd67244726b84af7b76f82bf64754a2536fbb3
dynilath <dynilath@qq.com>
2025-04-18 19:28:16 +0800

    chore: 添加一个启动本地BC脚本，调整构建脚本

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,9 @@
-eval(require("fs").readFileSync("./utils/rollup.tools.js", "utf8"));
+const path = require("path");
+const { createModRollupConfig, parseEnv } = require("./utils/rollupUtils.cjs");
+
+const packageJSON = require(path.join(process.cwd(), "package.json"));
+
+module.exports = async (cliArgs) => {
+    const env = parseEnv(__dirname, cliArgs);
+    return [await createModRollupConfig({ env, packageJSON })];
+};
