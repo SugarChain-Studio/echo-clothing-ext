a595e487422b2ad4387bd16d856c0a7642ac5709
dynilath <dynilath@qq.com>
2025-05-22 01:39:07 +0800

    refactor: 调整眼睛实现

--- src/components/衣服和装饰/化妆美发/新眼睛.js
+++ src/components/衣服和装饰/化妆美发/新眼睛.js
@@ -1,197 +1,285 @@
 import { AssetManager } from "../../../assetForward";
 
-/** @type {CustomAssetDefinitionAppearance[]} */
+/**
+ * @typedef {object} EyeDefinition
+ * @property {CustomAssetDefinitionAppearance} asset
+ * @property {Parameters<typeof AssetManager.addAssetWithConfig>[2]["translation"]} translation
+ * @property {Parameters<typeof AssetManager.addAssetWithConfig>[2]["layerNames"]} layerNames
+ */
+
+/**
+ * @param {EyeDefinition} eyeDef
+ */
+export function addEyeAsset(eyeDef) {
+    for (const group of /** @type {CustomGroupBodyName[]} */ (["左眼_Luzi", "右眼_Luzi"])) {
+        AssetManager.addAssetWithConfig(group, eyeDef.asset, {
+            translation: eyeDef.translation,
+            layerNames: eyeDef.layerNames,
+        });
+    }
+}
+
+const layerNames = {
+    CN: {
+        1: "眼睑",
+        2: "眼球",
+        3: "虹膜",
+        4: "亮斑",
+        5: "睫毛",
+        6: "睫毛（扩展）",
+    },
+    EN: {
+        1: "Eyelid",
+        2: "Eyeball",
+        3: "Iris",
+        4: "Highlight",
+        5: "Lash",
+        6: "Lash (Extended)",
+    },
+};
+
+/** @type {EyeDefinition[]} */
 const assets = [
     {
-        Name: "眼睛1",
-        Top: 120,
-        Left: 180,
-        // FullAlpha: false,
-        Layer: [
-            { Name: "1", AllowColorize: true },
-            { Name: "2", AllowColorize: true },
-            { Name: "3", AllowColorize: true },
-            { Name: "4", AllowColorize: true },
-        ],
+        asset: {
+            Name: "眼睛1",
+            Top: 120,
+            Left: 180,
+            Layer: [
+                { Name: "1", AllowColorize: true },
+                { Name: "2", AllowColorize: true },
+                { Name: "3", AllowColorize: true },
+                { Name: "4", AllowColorize: true },
+            ],
+        },
+        translation: {
+            CN: "红宝石",
+            EN: "Ruby",
+        },
+        layerNames: {
+            CN: {
+                1: "眼球",
+                2: "虹膜（暗部）",
+                3: "虹膜",
+                4: "睫毛",
+            },
+            EN: {
+                1: "Eyeball",
+                2: "Iris (Dark)",
+                3: "Iris",
+                4: "Lash",
+            },
+        },
     },
     {
-        Name: "眼睛2",
-        Top: 120,
-        Left: 180,
-        // DefaultColor: ["Default", "Default", "#404040", "#2F0707", "#000000", "#000000", "#000000", "#000000"],
-        Layer: [
-            { Name: "1", AllowColorize: true },
-            { Name: "2", AllowColorize: true },
-            { Name: "3", AllowColorize: true },
-            { Name: "4", AllowColorize: true },
-            { Name: "5", AllowColorize: true },
-            { Name: "6", AllowColorize: true },
-            { Name: "7", AllowColorize: true },
-            { Name: "8", AllowColorize: true },
-        ],
+        asset: {
+            Name: "眼睛2",
+            Top: 120,
+            Left: 180,
+            Layer: [
+                { Name: "1", AllowColorize: true },
+                { Name: "2", AllowColorize: true },
+                { Name: "3", AllowColorize: true },
+                { Name: "4", AllowColorize: true },
+                { Name: "5", AllowColorize: true },
+                { Name: "6", AllowColorize: true },
+                { Name: "7", AllowColorize: true },
+                { Name: "8", AllowColorize: true },
+            ],
+        },
+        translation: {
+            CN: "天狐",
+            EN: "Celestial Vixen",
+        },
+        layerNames: {
+            CN: {
+                1: "下眼睑",
+                2: "上眼睑",
+                3: "眼球",
+                4: "虹膜",
+                5: "虹膜边缘",
+                6: "瞳孔",
+                7: "下睫毛",
+                8: "上睫毛",
+            },
+            EN: {
+                1: "Lower Eyelid",
+                2: "Upper Eyelid",
+                3: "Eyeball",
+                4: "Iris",
+                5: "Iris Edge",
+                6: "Pupil",
+                7: "Lower Lash",
+                8: "Upper Lash",
+            },
+        },
     },
     {
-        Name: "眼睛3",
-        Top: 0,
-        Left: 0,
-        Layer: [
-            { Name: "1", AllowColorize: true },
-            { Name: "2", AllowColorize: true },
-            { Name: "3", AllowColorize: true },
-            { Name: "4", AllowColorize: true },
-            { Name: "5", AllowColorize: true },
-        ],
+        asset: {
+            Name: "眼睛3",
+            Top: 0,
+            Left: 0,
+            DefaultColor: ["Default", "Default", "#5AFFD1", "Default", "Default"],
+            Layer: [
+                { Name: "1", AllowColorize: true },
+                { Name: "2", AllowColorize: true },
+                { Name: "3", AllowColorize: true },
+                { Name: "4", AllowColorize: true },
+                { Name: "5", AllowColorize: true },
+            ],
+        },
+        translation: {
+            CN: "青玉",
+            EN: "Jade",
+        },
+        layerNames,
     },
     {
-        Name: "眼睛4",
-        Top: 0,
-        Left: 0,
-        Layer: [
-            { Name: "1", AllowColorize: true },
-            { Name: "2", AllowColorize: true },
-            { Name: "3", AllowColorize: true },
-            { Name: "4", AllowColorize: true },
-            { Name: "5", AllowColorize: true },
-        ],
+        asset: {
+            Name: "眼睛4",
+            Top: 0,
+            Left: 0,
+            DefaultColor: ["Default", "Default", "#9CFFF9", "Default", "Default"],
+            Layer: [
+                { Name: "1", AllowColorize: true },
+                { Name: "2", AllowColorize: true },
+                { Name: "3", AllowColorize: true },
+                { Name: "4", AllowColorize: true },
+                { Name: "5", AllowColorize: true },
+            ],
+        },
+        translation: {
+            CN: "水晶",
+            EN: "Crystal",
+        },
+        layerNames,
     },
     {
-        Name: "眼睛5",
-        Top: 0,
-        Left: 0,
-        Layer: [
-            { Name: "1", AllowColorize: true },
-            { Name: "2", AllowColorize: true },
-            { Name: "3", AllowColorize: true },
-            { Name: "4", AllowColorize: true },
-            { Name: "5", AllowColorize: true },
-        ],
+        asset: {
+            Name: "眼睛5",
+            Top: 0,
+            Left: 0,
+            DefaultColor: ["Default", "Default", "#9CFFF9", "Default", "Default"],
+            Layer: [
+                { Name: "1", AllowColorize: true },
+                { Name: "2", AllowColorize: true },
+                { Name: "3", AllowColorize: true },
+                { Name: "4", AllowColorize: true },
+                { Name: "5", AllowColorize: true },
+            ],
+        },
+        translation: {
+            CN: "水晶（长睫毛）",
+            EN: "Crystal (Long Lash)",
+        },
+        layerNames,
     },
     {
-        Name: "眼睛6",
-        Top: 0,
-        Left: 0,
-        Layer: [
-            { Name: "1", AllowColorize: true },
-            { Name: "2", AllowColorize: true },
-            { Name: "3", AllowColorize: true },
-            { Name: "4", AllowColorize: true },
-            { Name: "5", AllowColorize: true },
-            { Name: "6", AllowColorize: true },
-        ],
+        asset: {
+            Name: "眼睛6",
+            Top: 0,
+            Left: 0,
+            DefaultColor: ["Default", "Default", "#FFE695", "Default", "Default", "Default"],
+            Layer: [
+                { Name: "1", AllowColorize: true },
+                { Name: "2", AllowColorize: true },
+                { Name: "3", AllowColorize: true },
+                { Name: "4", AllowColorize: true },
+                { Name: "5", AllowColorize: true },
+                { Name: "6", AllowColorize: true },
+            ],
+        },
+        translation: {
+            CN: "黄玉",
+            EN: "Topaz",
+        },
+        layerNames,
     },
     {
-        Name: "眼睛7",
-        Top: 0,
-        Left: 0,
-        Layer: [
-            { Name: "1", AllowColorize: true },
-            { Name: "2", AllowColorize: true },
-            { Name: "3", AllowColorize: true },
-            { Name: "4", AllowColorize: true },
-            { Name: "5", AllowColorize: true },
-            { Name: "6", AllowColorize: true },
-        ],
+        asset: {
+            Name: "眼睛7",
+            Top: 0,
+            Left: 0,
+            DefaultColor: ["Default", "Default", "#9CFFF9", "Default", "Default"],
+            Layer: [
+                { Name: "1", AllowColorize: true },
+                { Name: "2", AllowColorize: true },
+                { Name: "3", AllowColorize: true },
+                { Name: "4", AllowColorize: true },
+                { Name: "5", AllowColorize: true },
+                { Name: "6", AllowColorize: true },
+            ],
+        },
+        translation: {
+            CN: "水晶（锐利）",
+            EN: "Crystal (Sharp)",
+        },
+        layerNames,
     },
     {
-        Name: "眼睛8",
-        Top: 0,
-        Left: 0,
-        Layer: [
-            { Name: "1", AllowColorize: true },
-            { Name: "2", AllowColorize: true },
-            { Name: "3", AllowColorize: true },
-            { Name: "4", AllowColorize: true },
-            { Name: "5", AllowColorize: true },
-        ],
+        asset: {
+            Name: "眼睛8",
+            Top: 0,
+            Left: 0,
+            DefaultColor: ["Default", "Default", "#5AFFD1", "Default", "Default"],
+            Layer: [
+                { Name: "1", AllowColorize: true },
+                { Name: "2", AllowColorize: true },
+                { Name: "3", AllowColorize: true },
+                { Name: "4", AllowColorize: true },
+                { Name: "5", AllowColorize: true },
+            ],
+        },
+        translation: {
+            CN: "青玉（睁大）",
+            EN: "Jade (Wide Open)",
+        },
+        layerNames,
     },
     {
-        Name: "眼睛9",
-        Top: 0,
-        Left: 0,
-        Layer: [
-            { Name: "1", AllowColorize: true },
-            { Name: "2", AllowColorize: true },
-            { Name: "3", AllowColorize: true },
-            { Name: "4", AllowColorize: true },
-            { Name: "5", AllowColorize: true },
-        ],
+        asset: {
+            Name: "眼睛9",
+            Top: 0,
+            Left: 0,
+            DefaultColor: ["Default", "Default", "#5AFFD1", "Default", "Default"],
+            Layer: [
+                { Name: "1", AllowColorize: true },
+                { Name: "2", AllowColorize: true },
+                { Name: "3", AllowColorize: true },
+                { Name: "4", AllowColorize: true },
+                { Name: "5", AllowColorize: true },
+            ],
+        },
+        translation: {
+            CN: "青玉（眼影）",
+            EN: "Jade (Eyeshadow)",
+        },
+        layerNames,
     },
     {
-        Name: "眼睛10",
-        Top: 0,
-        Left: 0,
-        Layer: [
-            { Name: "1", AllowColorize: true },
-            { Name: "2", AllowColorize: true },
-            { Name: "3", AllowColorize: true },
-            { Name: "4", AllowColorize: true },
-            { Name: "5", AllowColorize: true },
-        ],
+        asset: {
+            Name: "眼睛10",
+            Top: 0,
+            Left: 0,
+            DefaultColor: ["Default", "Default", "#5AFFD1", "Default", "Default"],
+            Layer: [
+                { Name: "1", AllowColorize: true },
+                { Name: "2", AllowColorize: true },
+                { Name: "3", AllowColorize: true },
+                { Name: "4", AllowColorize: true },
+                { Name: "5", AllowColorize: true },
+            ],
+        },
+        translation: {
+            CN: "青玉（眼影2）",
+            EN: "Jade (Eyeshadow 2)",
+        },
+        layerNames,
     },
 ];
 
-/** @type { Record<string,Translation.Entry> } */
-const translations = {
-    眼睛1: {
-        CN: "红宝石",
-        EN: "Ruby",
-    },
-    眼睛2: {
-        CN: "天狐",
-        EN: "Celestial Vixen",
-    },
-};
-
-/** @type { Translation.CustomRecord<string,string> } */
-const layerNames = {
-    CN: {
-        1: "下眼睑",
-        2: "上眼睑",
-        3: "眼球",
-        4: "虹膜",
-        5: "虹膜边缘",
-        6: "瞳孔",
-        7: "下睫毛",
-        8: "上睫毛",
-    },
-    EN: {
-        1: "Lower Eyelid",
-        2: "Upper Eyelid",
-        3: "Eyeball",
-        4: "Iris",
-        5: "Iris Edge",
-        6: "Pupil",
-        7: "Lower Lash",
-        8: "Upper Lash",
-    },
-};
-
-/** @type { CustomGroupedAssetDefinitions }} */
-const mirroredAssets = {
-    左眼_Luzi: assets,
-    右眼_Luzi: assets,
-};
-
-const groupedLayerNames = {
-    CN: { 左眼_Luzi: { 眼睛2: layerNames.CN }, 右眼_Luzi: { 眼睛2: layerNames.CN } },
-    EN: { 左眼_Luzi: { 眼睛2: layerNames.EN }, 右眼_Luzi: { 眼睛2: layerNames.EN } },
-};
-
-/** @type {CustomGroupName[]} */
-const eyeGroups = ["左眼_Luzi", "右眼_Luzi"];
-
-/** @type { Translation.GroupedEntries } */
-const mirroredTranslations = Object.entries(translations).reduce((acc, [name, value]) => {
-    for (const [lang, text] of Object.entries(value)) {
-        acc[lang] ??= {};
-        for (const group of eyeGroups) {
-            acc[lang][group] ??= {};
-            acc[lang][group][name] = text;
-        }
-    }
-    return acc;
-}, /** @type { Translation.GroupedEntries } */({}));
-
 export default function () {
-    AssetManager.addGroupedAssetsWithConfig(mirroredAssets, mirroredTranslations, groupedLayerNames);
+    for (const asset of assets) {
+        addEyeAsset(asset);
+    }
 }
