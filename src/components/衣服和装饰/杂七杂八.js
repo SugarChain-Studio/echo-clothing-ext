2e0ff706d9525cabbdb85c0e7b892a503c337c0d
Echo <1260556316@qq.com>
2025-06-16 10:08:20 +0800

    feat: 眼镜+2 (爱心眼镜,下半框眼镜)

--- src/components/衣服和装饰/杂七杂八.js
+++ src/components/衣服和装饰/杂七杂八.js
@@ -30,6 +30,25 @@ const assets = {
             Left: 0,
             Layer: [{ Name: "眼镜", Priority: 56 }],
         },
+        {
+            Name: "爱心眼镜_Luzi",
+            Random: false,
+            Top: 0,
+            Left: 0,
+            Priority: 56,
+            Layer: [
+                { Name: "1" },
+                { Name: "2" },
+                { Name: "3" },
+                { Name: "4" },
+            ],
+        },
+        {
+            Name: "下半框眼镜_Luzi",
+            Random: false,
+            Top: 0,
+            Left: 0,
+        },
     ],
     BodyMarkings: [
         {
@@ -53,6 +72,8 @@ const translations = {
             单边眼镜左_Luzi: "单边眼镜左",
             单边眼镜右_Luzi: "单边眼镜右",
             眼镜卡_Luzi: "眼镜卡",
+            爱心眼镜_Luzi: "爱心眼镜",
+            下半框眼镜_Luzi: "下半框眼镜",
         },
         BodyMarkings: {
             番茄酱_Luzi: "番茄酱",
@@ -66,6 +87,8 @@ const translations = {
             单边眼镜左_Luzi: "Monocle Left",
             单边眼镜右_Luzi: "Monocle Right",
             眼镜卡_Luzi: "Glasses Card",
+            爱心眼镜_Luzi: "Heart-shaped glasses",
+            下半框眼镜_Luzi: "Semi-rimless glasses",
         },
         BodyMarkings: {
             番茄酱_Luzi: "Ketchup",
