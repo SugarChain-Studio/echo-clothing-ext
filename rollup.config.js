a4971e88d1da9f1a92eedda08b6314c744498a12
dynilath <dynilath@qq.com>
2025-06-17 21:58:12 +0800

    build: 在构建输出中添加授权banner

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,25 @@
 const path = require("path");
 const { createModRollupConfig, parseEnv } = require("./utils/rollupUtils.cjs");
+const fs = require("fs");
 
 const packageJSON = require(path.join(process.cwd(), "package.json"));
 
+const repo = (() => {
+    let url = packageJSON.repository?.url;
+    if (!url) return "";
+
+    if (url.startsWith("git+")) {
+        url = url.slice(4);
+    }
+    if (url.endsWith(".git")) {
+        url = url.slice(0, -4);
+    }
+    return url;
+})();
+
+const banner = `/*\n${fs.readFileSync("LICENSE")}\n\n${repo}\n@preserve\n*/`;
+
 module.exports = async (cliArgs) => {
     const env = parseEnv(__dirname, cliArgs);
-    return [await createModRollupConfig({ env, packageJSON })];
+    return [await createModRollupConfig({ env, packageJSON, banner })];
 };
