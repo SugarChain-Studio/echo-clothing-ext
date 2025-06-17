17943c57209f18419253b7c837977c26571c9995
dynilath <dynilath@qq.com>
2025-06-17 17:52:46 +0800

    refactor: 使位置调整能适应身体组状态

--- src/components/修改身体组的位置.js
+++ src/components/修改身体组的位置.js
@@ -1,49 +1,35 @@
-import { AssetManager } from "../assetForward";
-import { Tools } from "@mod-utils/Tools";
+import { HookManager } from "@sugarch/bc-mod-hook-manager";
 
 export default function () {
     // 在加载完成之后，图层位置都直接写在图层属性里面了，所以这里直接调整图层位置
 
-    AssetManager.modifyAssetLayers(
-        (asset) => ["Pussy", "ItemVulva", "ItemVulvaPiercings", "ItemButt"].includes(asset.Group.Name),
-        (asset, layer) => {
-            // topLeftAdjust 是相对于原位置调整，如果一个图层被调整了很多次，那么调整会叠加
-            // 所以这里集中所有的调整，避免重复调整
+    HookManager.hookFunction("CommonDrawComputeDrawingCoordinates", 0, (args, next) => {
+        const ret = next(args);
+        const [C, asset] = args;
 
+        const bodyStyleItem = InventoryGet(C, /** @type {AssetGroupBodyName}*/ ("BodyStyle"));
+        if (bodyStyleItem?.Asset?.Name !== "Original") return ret;
+
+        if (["Pussy", "ItemVulva", "ItemVulvaPiercings", "ItemButt"].includes(asset.Group.Name)) {
             if (["FlatChastityCage", "PlasticChastityCage", "FuturisticTrainingBelt"].includes(asset.Name)) {
-                layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -20);
+                ret.Y -= 20;
             } else {
-                layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -16);
+                ret.Y -= 16;
+            }
+        } else if (asset.Group.Name === "Panties") {
+            if (["CockSock", "Jockstrap"].includes(asset.Name)) {
+                ret.Y -= 20;
+            }
+        } else if (asset.Group.Name === "ItemPelvis") {
+            if (asset.Name === "HarnessPanties1") {
+                ret.Y -= 20;
+            }
+        } else if (asset.Name === "StrictPonyBoots") {
+            if (C.PoseMapping.BodyUpper === "BaseLower") {
+                ret.Y -= 10;
             }
         }
-    );
 
-    AssetManager.modifyAssetLayers(
-        (asset) => asset.Group.Name === "Panties",
-        (asset, layer) => {
-            if (asset.Name === "CockSock") {
-                layer.DrawingTop = Tools.topLeftOverride(layer.DrawingTop, 430);
-            } else if (asset.Name === "Jockstrap") {
-                layer.DrawingTop = Tools.topLeftOverride(layer.DrawingTop, 393);
-            }
-        }
-    );
-
-    AssetManager.modifyAssetLayers(
-        (asset) => asset.Name === "StrictPonyBoots",
-        (asset, layer) => {
-            // 第二个参数可以指定姿势
-            // layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, { BaseLower: -10 });
-
-            // 也可以直接覆写
-            layer.DrawingTop = Tools.topLeftOverride(layer.DrawingTop, { BaseLower: 460 });
-        }
-    );
-
-    AssetManager.modifyAssetLayers(
-        (asset) => asset.Name === "HarnessPanties1",
-        (asset, layer) => {
-            layer.DrawingTop = Tools.topLeftOverride(layer.DrawingTop, 375);
-        }
-    );
+        return ret;
+    });
 }
