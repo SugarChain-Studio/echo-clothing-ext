58cef8f07391da446d7bb62158c8fdb6542515ee
Echo <1260556316@qq.com>
2025-06-19 21:18:27 +0800

    feat: 两个尾巴

--- src/components/衣服和装饰/尾巴_镜像.js
+++ src/components/衣服和装饰/尾巴_镜像.js
@@ -110,7 +110,26 @@ const assets = [
             { Name: "2" },
         ],
     },
-
+    {
+        Name: "鱼尾1_Luzi",
+        Random: false,
+        Top: 0,
+        Left: 0,
+        Layer: [
+            { Name: "1" },
+            { Name: "2" },
+        ],
+    },
+    {
+        Name: "鱼尾2_Luzi",
+        Random: false,
+        Top: 0,
+        Left: 0,
+        Layer: [
+            { Name: "1" },
+            { Name: "2" },
+        ],
+    },
 ];
 
 /** @type { Record<string,string> } */
@@ -151,6 +170,8 @@ const translations = {
             尾巴3_Luzi: "尾巴 3",
             雪豹尾巴_Luzi: "雪豹尾巴",
             雪豹尾巴镜像_Luzi: "雪豹尾巴镜像",
+            鱼尾1_Luzi: "鱼尾 1",
+            鱼尾2_Luzi: "鱼尾 2",
         },
     },
     EN: {
@@ -169,6 +190,8 @@ const translations = {
             尾巴3_Luzi: "Tail 3",
             雪豹尾巴_Luzi: "Wearable Snow Leopard Tail",
             雪豹尾巴镜像_Luzi: "Wearable Snow Leopard Tail",
+            鱼尾1_Luzi: "Fish Tail 1",
+            鱼尾2_Luzi: "Fish Tail 2",
         },
     },
     RU: {
