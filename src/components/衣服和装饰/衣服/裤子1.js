a8431fbb25a32e383fc958ec6c941b08a9f598fe
Echo <1260556316@qq.com>
2025-07-03 12:50:22 +0800

    feat: 添加裤子1

---
+++ src/components/衣服和装饰/衣服/裤子1.js
@@ -0,0 +1,41 @@
+import { AssetManager } from "../../../assetForward";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "裤子1_Luzi",
+    Random: false,
+    Top: 0,
+    Left: {
+        [PoseType.DEFAULT]: 0,
+        KneelingSpread: 90,
+    },
+    Layer: [
+        { Name: "右_C4" },
+        { Name: "右_C3" },
+        { Name: "右_C2" },
+        { Name: "右_C1" },
+        { Name: "右_B2" },
+        { Name: "右_B1" },
+        { Name: "右_A1" },
+        { Name: "右_A0", Priority: 1 },
+
+        { Name: "左_C4" },
+        { Name: "左_C3" },
+        { Name: "左_C2" },
+        { Name: "左_C1" },
+        { Name: "左_B2" },
+        { Name: "左_B1" },
+        { Name: "左_A1" },
+        { Name: "左_A0", Priority: 1 },
+    ],
+};
+
+/** @type {Translation.Entry} */
+const translation = {
+    CN: "裤子 1",
+    EN: "Pants 1",
+};
+
+export default function () {
+    AssetManager.addAsset("ClothLower", asset, undefined, translation);
+}
