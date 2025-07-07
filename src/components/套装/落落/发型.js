0121dca57ea7cf2950130a6c366d8f01e857b1a4
dynilath <dynilath@qq.com>
2025-07-08 02:06:54 +0800

    refactor: 实现蓬松卷发

---
+++ src/components/套装/落落/发型.js
@@ -0,0 +1,74 @@
+import { AssetManager } from "../../../assetForward";
+
+/** @type {"HairFront"} */
+const hairFront = "HairFront";
+
+const hairAttrShared = {
+    Random: false,
+    Left: 0,
+    Top: 0,
+    Priority: 52,
+    ParentGroup: {},
+    Extended: false,
+    InheritColor: hairFront,
+    Hide: [hairFront],
+};
+
+/** @type { CustomGroupedAssetDefinitions }} */
+const assets = {
+    新前发_Luzi: [
+        {
+            Name: "蓬松卷发1",
+            ...hairAttrShared,
+            Layer: [{ Name: "A" }, { Name: "B" }],
+        },
+        {
+            Name: "蓬松卷发2",
+            ...hairAttrShared,
+            Layer: [{ Name: "A" }, { Name: "B" }],
+        },
+    ],
+};
+
+/** @type { Translation.GroupedEntries } */
+const translations = {
+    CN: {
+        新前发_Luzi: {
+            蓬松卷发1: "蓬松卷发1",
+            蓬松卷发2: "蓬松卷发2",
+        },
+    },
+    EN: {
+        新前发_Luzi: {
+            蓬松卷发1: "Fluffy Curls 1",
+            蓬松卷发2: "Fluffy Curls 2",
+        },
+    },
+};
+
+const layerNames = {
+    CN: {
+        蓬松卷发1: {
+            A: "两侧",
+            B: "头顶",
+        },
+        蓬松卷发2: {
+            A: "两侧",
+            B: "头顶",
+        },
+    },
+    EN: {
+        蓬松卷发1: {
+            A: "Sides",
+            B: "Top",
+        },
+        蓬松卷发2: {
+            A: "Sides",
+            B: "Top",
+        },
+    },
+};
+
+export default function () {
+    AssetManager.addGroupedAssetsWithConfig(assets, translations, layerNames);
+}
