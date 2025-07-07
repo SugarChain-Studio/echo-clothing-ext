afa7191b608f1b32af2ba45b237786761230be38
Echo <1260556316@qq.com>
2025-07-07 22:51:17 +0800

    feat: 落落-俩鞋子

---
+++ src/components/衣服和装饰/鞋子/落落_靴子1.js
@@ -0,0 +1,36 @@
+import { AssetManager } from "../../../assetForward";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "靴子1_Luzi",
+    Random: false,
+    Top: 0,
+    Left: 0,
+    PoseMapping: {
+        Kneel: PoseType.HIDE,
+        KneelingSpread: PoseType.HIDE,
+        LegsClosed: "LegsClosed",
+        Spread: "Spread",
+        Hogtied: PoseType.HIDE,
+        AllFours: PoseType.HIDE,
+    },
+    Priority: 23,
+    Layer: [
+        {
+            Name: "右",
+        },
+        {
+            Name: "左",
+        },
+    ],
+};
+
+/** @type {Translation.Entry} */
+const translation = {
+    CN: "靴子 1",
+    EN: "Lace-up shoes",
+};
+
+export default function () {
+    AssetManager.addAsset("Shoes", asset, undefined, translation);
+}
