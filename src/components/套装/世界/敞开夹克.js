9ccda34ca7279ca9c5be9e17980ad15b16cec49a
Da'Inihlus <dynilath@qq.com>
2025-08-10 04:01:37 +0800

    refactor: 修正敞夹克图片

--- src/components/套装/世界/敞开夹克.js
+++ src/components/套装/世界/敞开夹克.js
@@ -12,21 +12,21 @@ const poseMapping = {
 const backPoses = ["BackBoxTie", "BackCuffs", "BackElbowTouch"];
 
 /**
- * @typedef {Pick<AssetLayerDefinition, "Name"| "CopyLayerColor" |"Priority"|"PoseMapping"> & {ColorName?:string}} MyDefinition
+ * @typedef {Pick<AssetLayerDefinition, "Name"| "CopyLayerColor" |"Priority"|"PoseMapping" | "AllowTypes"> & {ColorName?:string}} MyDefinition
  */
 
-const layerNameBase = {
+const layerGroupBase = {
     B: ["外侧", "拉链"],
     C: ["金属环", "饰带边缘", "饰带"],
     D: ["袖子", "内层", "内衬", "内衬图案", "拉链内侧"],
 };
 
 const layerDefBase = /** @type {MyDefinition[]} */ ([
-    ...iota(2, (i) => ({ Name: `B${i + 1}`, ColorName: layerNameBase.B[i] })),
-    ...iota(3, (i) => ({ Name: `C${i + 1}`, ColorName: layerNameBase.C[i] })),
+    ...iota(2, (i) => ({ Name: `B${i + 1}`, ColorName: layerGroupBase.B[i] })),
+    ...iota(3, (i) => ({ Name: `C${i + 1}`, ColorName: layerGroupBase.C[i] })),
     {
         Name: "D1",
-        ColorName: layerNameBase.D[0],
+        ColorName: layerGroupBase.D[0],
         PoseMapping: {
             ...poseMapping,
             ...Object.fromEntries(backPoses.map((pose) => [pose, PoseType.HIDE])),
@@ -41,28 +41,47 @@ const layerDefBase = /** @type {MyDefinition[]} */ ([
             ...Object.fromEntries(backPoses.map((pose) => [pose, pose])),
         },
     },
-    ...iota(4, (i) => ({ Name: `D${i + 2}`, ColorName: layerNameBase.D[i + 1] })),
-]).flatMap((layer) =>
-    ["L", "R"].flatMap((D) => [
+    ...iota(4, (i) => ({ Name: `D${i + 2}`, ColorName: layerGroupBase.D[i + 1] })),
+])
+    .flatMap((layer) =>
+        ["L", "R"].flatMap(
+            (D) =>
+                /** @type {MyDefinition[]} */ ([
+                    {
+                        ...layer,
+                        Name: `Y_${D}_${layer.Name}`,
+                        ColorName: D === "L" ? "左" : "右",
+                        ColorGroup: layer.ColorName,
+                        ...(layer.CopyLayerColor ? { CopyLayerColor: `Y_${D}_${layer.CopyLayerColor}` } : {}),
+                        AllowTypes: { typed: 0 },
+                    },
+                    {
+                        ...layer,
+                        Name: `X_${D}_${layer.Name}`,
+                        ColorName: D === "L" ? "左" : "右",
+                        ...(layer.CopyLayerColor
+                            ? { CopyLayerColor: `Y_${D}_${layer.CopyLayerColor}` }
+                            : { CopyLayerColor: `Y_${D}_${layer.Name}` }),
+                        AllowTypes: { typed: 1 },
+                    },
+                ])
+        )
+    )
+    .concat([
         {
-            ...layer,
-            Name: `Y_${D}_${layer.Name}`,
-            ColorName: D === "L" ? "左" : "右",
-            ColorGroup: layer.ColorName,
-            ...(layer.CopyLayerColor ? { CopyLayerColor: `Y_${D}_${layer.CopyLayerColor}` } : {}),
+            Name: "Y_A1",
+            ColorName: "后背",
+            Priority: 5,
             AllowTypes: { typed: 0 },
         },
         {
-            ...layer,
-            Name: `X_${D}_${layer.Name}`,
-            ColorName: D === "L" ? "左" : "右",
-            ...(layer.CopyLayerColor
-                ? { CopyLayerColor: `Y_${D}_${layer.CopyLayerColor}` }
-                : { CopyLayerColor: `Y_${D}_${layer.Name}` }),
+            Name: "X_A1",
+            ColorName: "后背",
+            CopyLayerColor: "Y_A1",
+            Priority: 5,
             AllowTypes: { typed: 1 },
         },
-    ])
-);
+    ]);
 
 /** @type {CustomAssetDefinition} */
 const asset = {
@@ -112,6 +131,8 @@ const layerNames = {
     },
     EN: {
         ...Object.fromEntries(layerDefBase.filter((l) => l.ColorName).map((l) => [l.Name, ENlayer[l.ColorName]])),
+        Y_A1: "Back",
+
         外侧: "Outer",
         拉链: "Zipper",
         金属环: "Metal Ring",
