05d7a1810d10e66ff43ed092ce623234a864efe5
dynilath <dynilath@qq.com>
2025-08-24 18:01:39 +0800

    refactor: 调整 takeLayerNames 函数位置

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,3 +3,4 @@ export { CraftingCache } from "./craftingCache.js";
 export { ExtendedTools } from "./extendedTools.js";
 export { Typing } from "./typing.js";
 export { SockLRTool } from "./sockLR.js";
+export { takeLayerNames } from "./takeLayerName.js";
--- src/lib/typing.js
+++ src/lib/typing.js
@@ -1,20 +1,3 @@
-/**
- * 从已有的物品定义中获取图层名称，组成字典。ColorGroup也会提取。类似 _1 和 _Luzi 这样的后缀会被清理。
- * @param {CustomAssetDefinition} assetDef
- * @return {Record<string,string>}
- */
-export function takeLayerNames(assetDef) {
-    /** @type {Record<string,string>} */
-    const ret = {};
-    assetDef.Layer?.filter((l) => (l.AllowColorize ?? true) && !l.CopyLayerColor && !l.HideColoring).forEach(
-        ({ Name, ColorGroup }) => {
-            ret[Name] = Name.replace(/(_\d+|_\w+)$/, "");
-            if (ColorGroup) ret[ColorGroup] = ColorGroup.replace(/(_\d+|_\w+)$/, "");
-        }
-    );
-    return ret;
-}
-
 /** @type {(arg:any)=>any} */
 const identity = (arg) => arg;
 
