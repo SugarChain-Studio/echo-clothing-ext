e7df17e9b5d1acc95f64735b3a3fee76293dd1ad
dynilath <dynilath@qq.com>
2025-08-29 01:59:35 +0800

    refactor: 更新花边连衣裙的颜色组定义和翻译文本

--- src/components/衣服和装饰/衣服/花边连衣裙.js
+++ src/components/衣服和装饰/衣服/花边连衣裙.js
@@ -30,6 +30,7 @@ const asset = {
     Layer: [
         {
             Name: "袖左",
+            ColorGroup: "袖子花边",
             PoseMapping: {
                 Yoked: "Yoked",
                 OverTheHead: "OverTheHead",
@@ -39,6 +40,7 @@ const asset = {
         },
         {
             Name: "袖右",
+            ColorGroup: "袖子花边",
             PoseMapping: {
                 Yoked: "Yoked",
                 OverTheHead: "OverTheHead",
@@ -48,15 +50,15 @@ const asset = {
         },
         { Name: "裙边2", ParentGroup: {} },
         { Name: "裙边", ParentGroup: {} },
-        { Name: "裙" },
-        { Name: "胸罩" },
-        { Name: "束腰左", ColorGroup: "束腰" },
-        { Name: "束腰右", ColorGroup: "束腰" },
-        { Name: "束腰中", ColorGroup: "束腰" },
+        { Name: "裙", ColorGroup: "裙子" },
+        { Name: "胸罩", ColorGroup: "裙子" },
+        { Name: "束腰左", ColorGroup: "裙子" },
+        { Name: "束腰右", ColorGroup: "裙子" },
+        { Name: "束腰中", ColorGroup: "裙子" },
         { Name: "束腰绑带" },
-        { Name: "腰带" },
+        { Name: "腰带", ColorGroup: "蝴蝶结" },
         { Name: "花边" },
-        { Name: "蝴蝶结", ParentGroup: {} },
+        { Name: "蝴蝶结", ColorGroup: "蝴蝶结", ParentGroup: {} },
     ],
 };
 
@@ -68,20 +70,39 @@ const translation = {
 
 /** @type {Translation.String} */
 const layerNames = {
+    CN: {
+        袖左: "左",
+        袖右: "右",
+
+        裙: "裙摆",
+        束腰左: "腰封左",
+        束腰右: "腰封右",
+        束腰中: "腰封中",
+
+        裙边2: "裙边(上)",
+        裙边: "裙边(下)",
+
+        腰带: "蝴蝶结缎带",
+
+        花边: "文胸花边",
+    },
     EN: {
-        袖左: "Left Sleeve",
-        袖右: "Right Sleeve",
-        裙边2: "Skirt Edge 2",
-        裙边: "Skirt Edge",
-        裙: "Skirt",
+        袖子花边: "Sleeve Frill",
+        袖左: "Left",
+        袖右: "Right",
+
+        裙子: "Skirt",
         胸罩: "Bra",
-        束腰: "Waist",
         束腰左: "Waist Left",
         束腰右: "Waist Right",
         束腰中: "Waist Middle",
+        裙: "Skirt",
+
+        裙边2: "Skirt Edge (Lower)",
+        裙边: "Skirt Edge (Upper)",
         束腰绑带: "Waist Strap",
-        腰带: "Belt",
-        花边: "Lace",
+        腰带: "Ribbon",
+        花边: "Bra Frill",
         蝴蝶结: "Bow",
     },
 };
