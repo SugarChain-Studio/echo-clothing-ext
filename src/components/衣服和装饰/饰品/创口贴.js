1c638cb5bba7a258d2e60c141d309b7790f38521
dynilath <dynilath@qq.com>
2025-08-30 08:26:00 +0800

    feat: 创口贴

---
+++ src/components/衣服和装饰/饰品/创口贴.js
@@ -0,0 +1,70 @@
+import { AssetManager } from "../../../assetForward";
+
+/** @type {CustomAssetDefinitionAppearance} */
+const baseAsset = {
+    Name: "创口贴",
+    Random: false,
+    ParentGroup: {},
+    DynamicGroupName: /** @type {"Suit"}*/ ("Suit"),
+    Layer: [
+        { Left: 180, Top: 300, Name: "上", ParentGroup: "BodyUpper" },
+        { Left: 240, Top: 500, Name: "下" },
+    ],
+};
+
+const lowerAsset = {
+    Name: "创口贴_下",
+    Random: false,
+    Left: 240,
+    Top: 500,
+    ParentGroup: {},
+    DynamicGroupName: /** @type {"Suit"}*/ ("Suit"),
+    PoseMapping: { AllFours: PoseType.HIDE, Hogtied: PoseType.HIDE },
+};
+
+const upperAsset = {
+    Name: "创口贴",
+    Random: false,
+    Left: 180,
+    Top: 300,
+    ParentGroup: /** @type {"BodyUpper"}*/ ("BodyUpper"),
+    DynamicGroupName: /** @type {"Suit"}*/ ("Suit"),
+    Layer: [{ Name: "上" }],
+};
+
+/** @type { CustomGroupedAssetDefinitions } */
+const asset = {
+    Suit: [baseAsset],
+    Bra: [upperAsset],
+    Panties: [{ ...lowerAsset, Expose: ["ItemVulvaPiercings", "ItemButt"] }],
+    ItemNipples: [upperAsset],
+    ItemVulva: [lowerAsset],
+    ItemVulvaPiercings: [{ ...lowerAsset, Block: ["ItemVulva"] }],
+};
+
+/** @type {Translation.GroupedEntries} */
+const translations = {
+    CN: Object.fromEntries(
+        Object.entries(asset).map(([group, assets]) => [
+            group,
+            Object.fromEntries(assets.map((asset) => [asset.Name, "创口贴"])),
+        ])
+    ),
+    EN: Object.fromEntries(
+        Object.entries(asset).map(([group, assets]) => [
+            group,
+            Object.fromEntries(assets.map((asset) => [asset.Name, "BandAid"])),
+        ])
+    ),
+};
+
+const layerNames = {
+    EN: { Suit: { 创口贴: { 上: "Top", 下: "Bottom" } } },
+};
+
+export default function () {
+    AssetManager.addImageMapping({
+        "Assets/Female3DCG/Suit/Preview/创口贴_下.png": "Assets/Female3DCG/Suit/Preview/创口贴.png",
+    });
+    AssetManager.addGroupedAssetsWithConfig(asset, translations, layerNames);
+}
