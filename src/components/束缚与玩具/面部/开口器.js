271133a642d2d471690da29883a9f8eb31c3b2f5
dynilath <dynilath@qq.com>
2025-09-01 02:04:31 +0800

    feat: 开口器 by 莓

---
+++ src/components/束缚与玩具/面部/开口器.js
@@ -0,0 +1,39 @@
+import { AssetManager } from "../../../assetForward";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "Luzi_CheekRetractor",
+    Random: false,
+    Left: 220,
+    Top: 180,
+    Difficulty: 6,
+    Time: 15,
+    AllowLock: false,
+    AllowTighten: true,
+    Fetish: ["Leather", "Metal", "Gagged"],
+    Hide: ["Mouth"],
+    Block: [],
+    Layer: [{ Name: "Lips" }, { Name: "Teeth" }, { Name: "Retractor" }],
+};
+
+const translation = {
+    CN: "开口器",
+    EN: "Cheek Retractor",
+};
+
+const layerNames = {
+    CN: {
+        Lips: "嘴唇",
+        Teeth: "牙齿",
+        Retractor: "开口器",
+    },
+    EN: {
+        Lips: "Lips",
+        Teeth: "Teeth",
+        Retractor: "Retractor",
+    },
+};
+
+export default function () {
+    AssetManager.addAssetWithConfig("ItemMouth", asset, { translation, layerNames });
+}
