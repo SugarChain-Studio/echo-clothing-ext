4ee9b73704aaa87d8866ae264c047298e41914fe
dynilath <dynilath@qq.com>
2025-09-09 14:25:03 +0800

    refactor: 修正左右袜子生成器逻辑

--- src/lib/sockLR.js
+++ src/lib/sockLR.js
@@ -1,7 +1,19 @@
-/** @type {Record<"SocksLeft" | "SocksRight", Alpha.Definition>} */
-const mask = {
-    SocksLeft: { Group: ["SocksLeft"], Masks: [[0, 0, 250, 1000]] },
-    SocksRight: { Group: ["SocksRight"], Masks: [[250, 0, 250, 1000]] },
+import { ImageMapTools } from "@mod-utils/Tools";
+import { AssetManager } from "../assetForward";
+
+const blackFill = (() => {
+    const canvas = document.createElement("canvas");
+    canvas.width = 250;
+    canvas.height = 1000;
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    return canvas.toDataURL();
+})();
+
+const topLeft = {
+    SocksLeft: { Top: 0, Left: { "": 0, "KneelingSpread": 30 } },
+    SocksRight: { Top: 0, Left: { "": 250, "KneelingSpread": 220 } },
 };
 
 /**
@@ -12,10 +24,29 @@ const mask = {
  */
 function createSockLR(asset, group = "Socks") {
     if (!Array.isArray(asset.Layer)) asset.Layer = [{}];
+
+    const maskName = `Mask${asset.Name}`;
+
+    const imageMap = /** @type {AssetGroupName[]}*/ (
+        asset.DynamicGroupName ? [asset.DynamicGroupName] : ["SocksLeft", "SocksRight"]
+    ).reduce((pv, cv) => {
+        pv[ImageMapTools.assetLayer(cv, `${asset.Name}_${maskName}`)] = blackFill;
+        return pv;
+    }, {});
+
+    AssetManager.addImageMapping(imageMap);
+
     return /** @type {[["SocksLeft", CustomAssetDefinition], ["SocksRight", CustomAssetDefinition]]} */ (
         /** @type {["SocksLeft", "SocksRight"]}*/ (["SocksLeft", "SocksRight"]).map((grp) => {
-            const ret = { ...asset, DynamicGroupName: group };
-            ret.Layer.push({ Name: grp, Top: 0, Left: { "": 0, "KneelingSpread": 30 }, Alpha: [mask[grp]] });
+            const ret = { ...asset, Layer: [...asset.Layer], DynamicGroupName: group };
+            ret.Layer.push({
+                Name: maskName,
+                ...topLeft[grp],
+                ParentGroup: {},
+                PoseMapping: {},
+                TextureMask: {},
+                BlendingMode: "destination-out",
+            });
             return [grp, ret];
         })
     );
