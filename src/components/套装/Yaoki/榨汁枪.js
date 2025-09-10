babc6aeb0233e92845da68062550eb66f6335a12
dynilath <dynilath@qq.com>
2025-09-11 03:27:55 +0800

    feat: 榨汁枪

---
+++ src/components/套装/Yaoki/榨汁枪.js
@@ -0,0 +1,50 @@
+import { ArmMaskTool } from "../../../lib";
+import { AssetManager } from "../../../assetForward";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "榨汁枪",
+    Random: false,
+    Left: 110,
+    Top: 360,
+    ParentGroup: {},
+    AllowActivity: ["MasturbateItem"],
+    Fetish: ["Sadism"],
+    Layer: [
+        { Name: "dc" },
+        { Name: "gc", BlendingMode: "screen", AllowColorize: false },
+        { Name: "dg" },
+        { Name: "gg", BlendingMode: "screen", AllowColorize: false },
+        { Name: "l" },
+    ],
+};
+
+const layerNames = {
+    CN: {
+        dc: "杯基本",
+        gc: "杯光泽",
+        dg: "枪基本",
+        gg: "枪光泽",
+        l: "灯光",
+    },
+    EN: {
+        dc: "Cup Base",
+        gc: "Cup Gloss",
+        dg: "Gun Base",
+        gg: "Gun Gloss",
+        l: "Light",
+    },
+};
+
+const translation = {
+    CN: "榨汁枪",
+    EN: "Cup Gun",
+};
+
+export default function () {
+    // ArmMaskTool.createArmMaskForCloth("ItemHandheld", asset, "Right");
+    AssetManager.addAssetWithConfig("ItemHandheld", asset, {
+        layerNames,
+        translation,
+    });
+}
