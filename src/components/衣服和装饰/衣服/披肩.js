10040492c426ab4c57067e2d11c6c5d325d38cac
dynilath <dynilath@qq.com>
2025-09-15 13:20:46 +0800

    refactor: 简化并重构披肩

--- src/components/衣服和装饰/衣服/披肩.js
+++ src/components/衣服和装饰/衣服/披肩.js
@@ -1,8 +1,9 @@
 import { AssetManager } from "../../../assetForward";
 
-/** @type { CustomGroupedAssetDefinitions }} */
-const assets = {
-    Cloth: [
+/** @type {AddAssetWithConfigParams[]} */
+const assets = [
+    [
+        "Cloth",
         {
             Name: "披肩_Luzi",
             Random: false,
@@ -28,128 +29,93 @@ const assets = {
                 },
             ],
         },
+        {
+            translation: {
+                CN: "天主披肩",
+                EN: "Christian Shawl",
+                RU: "Палантин",
+                UA: "Палантин",
+            },
+            layerNames: {
+                CN: { Band: "披肩带", Shawl: "披肩" },
+                EN: { Band: "Band", Shawl: "Shawl" },
+            },
+        },
     ],
-    ClothAccessory: [
+    [
+        "ClothAccessory",
         {
             Name: "披肩短_Luzi",
             Random: false,
             Gender: "F",
-            Top: 0,
-            Left: 0,
+            Left: 110,
+            Top: 160,
+            Priority: 34,
+            PoseMapping: {
+                Yoked: "Yoked",
+                OverTheHead: "OverTheHead",
+            },
             Prerequisite: ["HasBreasts"],
             ParentGroup: "BodyUpper",
             DefaultColor: ["#490707", "#490707", "#FFFFFF", "#FFFFFF"],
             Layer: [
-                {
-                    Name: "衣左",
-                    ColorGroup: "衣",
-                    Priority: 34,
-                    PoseMapping: {
-                        Yoked: "Yoked",
-                        OverTheHead: "OverTheHead",
-                    },
-                },
-                {
-                    Name: "衣右",
-                    ColorGroup: "衣",
-                    Priority: 34,
-                    PoseMapping: {
-                        Yoked: "Yoked",
-                        OverTheHead: "OverTheHead",
-                    },
-                },
-                {
-                    Name: "绒左",
-                    ColorGroup: "绒",
-                    Priority: 34,
-                    PoseMapping: {
-                        Yoked: "Yoked",
-                        OverTheHead: "OverTheHead",
-                    },
-                },
-                {
-                    Name: "绒右",
-                    ColorGroup: "绒",
-                    Priority: 34,
-                    PoseMapping: {
-                        Yoked: "Yoked",
-                        OverTheHead: "OverTheHead",
-                    },
-                },
+                { Name: "衣左", ColorGroup: "衣" },
+                { Name: "衣右", ColorGroup: "衣" },
+                { Name: "绒左", ColorGroup: "绒" },
+                { Name: "绒右", ColorGroup: "绒" },
             ],
         },
+        {
+            translation: {
+                CN: "圣诞披肩短",
+                EN: "Xmas Short Shawl",
+                RU: "Короткий палантин",
+                UA: "Короткий палантин",
+            },
+            layerNames: {
+                CN: { 衣左: "左", 衣右: "右", 绒左: "左", 绒右: "右", 衣: "披风", 绒: "绒毛" },
+                EN: { 衣左: "Left", 衣右: "Right", 绒左: "Left", 绒右: "Right", 衣: "Cloth", 绒: "Fur" },
+            },
+        },
+    ],
+    [
+        "ClothAccessory",
         {
             Name: "披肩长_Luzi",
             Random: false,
             Gender: "F",
-            Top: 0,
-            Left: 0,
+            Left: 80,
+            Top: 130,
+            Priority: 32,
+            PoseMapping: {
+                Yoked: "Yoked",
+                OverTheHead: "OverTheHead",
+            },
             Prerequisite: ["HasBreasts"],
             ParentGroup: "BodyUpper",
             DefaultColor: ["#490707", "#490707", "#FFFFFF", "#FFFFFF"],
             Layer: [
-                {
-                    Name: "衣左",
-                    ColorGroup: "衣",
-                    Priority: 32,
-                    PoseMapping: {
-                        TapedHands: PoseType.DEFAULT,
-                        Yoked: "Yoked",
-                        OverTheHead: "OverTheHead",
-                        BackBoxTie: PoseType.DEFAULT,
-                        BackElbowTouch: PoseType.DEFAULT,
-                        BackCuffs: PoseType.DEFAULT,
-                        AllFours: PoseType.DEFAULT,
-                        Hogtied: PoseType.DEFAULT,
-                    },
-                },
-                {
-                    Name: "衣右",
-                    ColorGroup: "衣",
-                    Priority: 32,
-                    PoseMapping: {
-                        TapedHands: PoseType.DEFAULT,
-                        Yoked: "Yoked",
-                        OverTheHead: "OverTheHead",
-                        BackBoxTie: PoseType.DEFAULT,
-                        BackElbowTouch: PoseType.DEFAULT,
-                        BackCuffs: PoseType.DEFAULT,
-                        AllFours: PoseType.DEFAULT,
-                        Hogtied: PoseType.DEFAULT,
-                    },
-                },
-                {
-                    Name: "绒左",
-                    ColorGroup: "绒",
-                    Priority: 32,
-                    PoseMapping: {
-                        TapedHands: PoseType.DEFAULT,
-                        Yoked: "Yoked",
-                        OverTheHead: "OverTheHead",
-                        BackBoxTie: PoseType.DEFAULT,
-                        BackElbowTouch: PoseType.DEFAULT,
-                        BackCuffs: PoseType.DEFAULT,
-                        AllFours: PoseType.DEFAULT,
-                        Hogtied: PoseType.DEFAULT,
-                    },
-                },
-                {
-                    Name: "绒右",
-                    ColorGroup: "绒",
-                    Priority: 32,
-                    PoseMapping: {
-                        TapedHands: PoseType.DEFAULT,
-                        Yoked: "Yoked",
-                        OverTheHead: "OverTheHead",
-                        BackBoxTie: PoseType.DEFAULT,
-                        BackElbowTouch: PoseType.DEFAULT,
-                        BackCuffs: PoseType.DEFAULT,
-                        AllFours: PoseType.DEFAULT,
-                        Hogtied: PoseType.DEFAULT,
-                    },
-                },
+                { Name: "衣左", ColorGroup: "衣" },
+                { Name: "衣右", ColorGroup: "衣" },
+                { Name: "绒左", ColorGroup: "绒" },
+                { Name: "绒右", ColorGroup: "绒" },
             ],
         },
+        {
+            translation: {
+                CN: "圣诞披肩长",
+                EN: "Xmas Long Shawl",
+                RU: "Длинный палантин",
+                UA: "Довгий палантин",
+            },
+            layerNames: {
+                CN: { 衣左: "左", 衣右: "右", 绒左: "左", 绒右: "右", 衣: "披风", 绒: "绒毛" },
+                EN: { 衣左: "Left", 衣右: "Right", 绒左: "Left", 绒右: "Right", 衣: "Cloth", 绒: "Fur" },
+            },
+        },
+    ],
+    [
+        "ClothAccessory",
         {
             Name: "立领披肩_Luzi",
             Random: false,
@@ -178,51 +144,21 @@ const assets = {
                 },
             ],
         },
+        {
+            translation: {
+                CN: "血族斗篷",
+                EN: "Vampiric Cloak",
+            },
+            layerNames: {
+                CN: { 披肩: "披肩", 披肩后: "披肩后" },
+                EN: { 披肩: "Shawl", 披肩后: "Shawl Back" },
+            },
+        },
     ],
-};
-
-/** @type { Translation.GroupedEntries } */
-const translations = {
-    CN: {
-        Cloth: {
-            披肩_Luzi: "天主披肩",
-        },
-        ClothAccessory: {
-            披肩短_Luzi: "圣诞披肩短",
-            披肩长_Luzi: "圣诞披肩长",
-            立领披肩_Luzi: "血族斗篷",
-        },
-    },
-    EN: {
-        Cloth: {
-            披肩_Luzi: "Christian Shawl",
-        },
-        ClothAccessory: {
-            披肩短_Luzi: "Xmas Short Shawl",
-            披肩长_Luzi: "Xmas Long Shawl",
-            立领披肩_Luzi: "Vampiric Cloak",
-        },
-    },
-    RU: {
-        Cloth: {
-            披肩_Luzi: "Палантин",
-        },
-        ClothAccessory: {
-            披肩短_Luzi: "Короткий палантин",
-            披肩长_Luzi: "Длинный палантин",
-        },
-    },
-    UA: {
-        Cloth: {
-            披肩_Luzi: "Палантин",
-        },
-        ClothAccessory: {
-            披肩短_Luzi: "Короткий палантин",
-            披肩长_Luzi: "Довгий палантин",
-        },
-    },
-};
+];
 
 export default function () {
-    AssetManager.addGroupedAssets(assets, translations);
+    for (const param of assets) {
+        AssetManager.addAssetWithConfig(...param);
+    }
 }
