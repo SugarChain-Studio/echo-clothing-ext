950003499f8722e875c7c5d4815befda35871f00
dynilath <dynilath@qq.com>
2025-09-20 20:37:15 +0800

    refactor: 调整和简化乳胶头套，能够隐藏耳朵

--- src/components/束缚与玩具/面部/乳胶头套.js
+++ src/components/束缚与玩具/面部/乳胶头套.js
@@ -1,4 +1,4 @@
-import { Tools } from "@mod-utils/Tools";
+import { DialogTools, Tools } from "@mod-utils/Tools";
 import { AssetManager } from "../../../assetForward";
 
 /** @type {CustomAssetDefinition} */
@@ -14,6 +14,16 @@ const asset = {
     AllowTighten: true,
     Fetish: ["Leather"],
     Block: [],
+    Alpha: [
+        {
+            Group: ["Head"],
+            Masks: [
+                [194, 162, 24, 32],
+                [282, 162, 24, 32],
+            ],
+            AllowTypes: { AC: 0 },
+        },
+    ],
 };
 
 /**@type {AssetArchetypeConfig} */
@@ -26,14 +36,7 @@ const extended = {
             Name: "透光度",
             DrawImages: false,
             Key: "l",
-            Options: [
-                {},
-                {
-                    Property: {
-                        Effect: [E.BlindHeavy, E.DeafLight, E.BlockWardrobe],
-                    },
-                },
-            ],
+            Options: [{}, { Property: { Effect: [E.BlindHeavy, E.DeafLight, E.BlockWardrobe] } }],
         },
         {
             Name: "隐藏前发",
@@ -45,127 +48,70 @@ const extended = {
             Name: "隐藏后发",
             DrawImages: false,
             Key: "B",
-            Options: [
-                {},
-                {
-                    Property: {
-                        Hide: [
-                            "HairBack",
-                            // @ts-ignore
-                            "新后发_Luzi",
-                        ],
-                    },
-                },
-            ],
+            Options: [{}, { Property: { Hide: ["HairBack"] } }],
         },
         {
             Name: "隐藏其他",
             DrawImages: false,
             Key: "AC",
-            Options: [
-                {},
-                {
-                    Property: {
-                        Hide: ["HairAccessory1", "HairAccessory2", "HairAccessory3"],
-                    },
-                },
-            ],
+            Options: [{}, { Property: { Hide: ["HairAccessory1", "HairAccessory2", "HairAccessory3"] } }],
         },
         {
             Name: "图层",
             DrawImages: false,
             Key: "P",
-            Options: [
-                {},
-                {
-                    Property: {
-                        OverridePriority: 12,
-                    },
-                },
-            ],
+            Options: [{}, { Property: { OverridePriority: 12 } }],
         },
     ],
 };
 
 /**@type {Translation.Dialog} */
-const assetStrings = {
-    CN: {
-        SelectBase: "透光度",
-        Module透光度: "透光度",
+const assetStrings = DialogTools.combine(
+    {
+        CN: {
+            SelectBase: "透光度",
+            Module透光度: "透光度",
 
-        Select透光度: "设置透光度",
-        Optionl0: "透光",
-        Optionl1: "不透光",
-        Setl0: "SourceCharacter使DestinationCharacterAssetName变得透明",
-        Setl1: "SourceCharacter使DestinationCharacterAssetName变得不透明",
+            Select透光度: "设置阻挡视线",
+            Optionl0: "不阻挡视线",
+            Optionl1: "阻挡视线",
+            Setl0: "SourceCharacter使DestinationCharacterAssetName不阻挡视线",
+            Setl1: "SourceCharacter使DestinationCharacterAssetName阻挡视线",
 
-        Module隐藏前发: "隐藏前发",
-        Select隐藏前发: "设置隐藏前发",
-        OptionF0: "显示",
-        OptionF1: "隐藏",
-        SetF0: "SourceCharacter使DestinationCharacterAssetName会露出前发",
-        SetF1: "SourceCharacter使DestinationCharacterAssetName会覆盖前发",
+            Module图层: "佩戴位置",
+            Select图层: "设置佩戴位置",
+            OptionP0: "在头部物品上方",
+            OptionP1: "在头部物品下方",
+            SetP0: "SourceCharacter使DestinationCharacterAssetName在头部物品上方",
+            SetP1: "SourceCharacter使DestinationCharacterAssetName在头部物品下方",
+        },
+        EN: {
+            SelectBase: "Transparency",
 
-        Module隐藏后发: "隐藏后发",
-        Select隐藏后发: "设置隐藏后发",
-        OptionB0: "显示",
-        OptionB1: "隐藏",
-        SetB0: "SourceCharacter使DestinationCharacterAssetName会露出后发",
-        SetB1: "SourceCharacter使DestinationCharacterAssetName会覆盖后发",
+            Module透光度: "Hinder Vision",
+            Select透光度: "Set Hinder Vision",
+            Optionl0: "Not Hinder",
+            Optionl1: "Hinder",
+            Setl0: "SourceCharacter makes DestinationCharacter AssetName not hinder vision.",
+            Setl1: "SourceCharacter makes DestinationCharacter AssetName hinder vision.",
 
-        Module隐藏其他: "隐藏其他头饰",
-        Select隐藏其他: "设置隐藏耳朵和其他头饰",
-        OptionAC0: "显示",
-        OptionAC1: "隐藏",
-        SetAC0: "SourceCharacter使DestinationCharacterAssetName会露出耳朵和其他头饰",
-        SetAC1: "SourceCharacter使DestinationCharacterAssetName会覆盖耳朵和其他头饰",
-
-        Module图层: "佩戴位置",
-        Select图层: "设置佩戴位置",
-        OptionP0: "在头部物品上方",
-        OptionP1: "在头部物品下方",
-        SetP0: "SourceCharacter使DestinationCharacterAssetName在头部物品上方",
-        SetP1: "SourceCharacter使DestinationCharacterAssetName在头部物品下方",
+            Module图层: "Wearing Position",
+            Select图层: "Set Wearing Position",
+            OptionP0: "Above Head Items",
+            OptionP1: "Below Head Items",
+            SetP0: "SourceCharacter makes DestinationCharacter AssetName above head items",
+            SetP1: "SourceCharacter makes DestinationCharacter AssetName below head items",
+        },
     },
-    EN: {
-        SelectBase: "Transparency",
-
-        Module透光度: "Transparency",
-        Select透光度: "Set Transparency",
-        Optionl0: "Transparent",
-        Optionl1: "Opaque",
-        Setl0: "SourceCharacter makes DestinationCharacter AssetName transparent.",
-        Setl1: "SourceCharacter makes DestinationCharacter AssetName opaque.",
-
-        Module隐藏前发: "Hide Front Hair",
-        Select隐藏前发: "Set Hide Front Hair",
-        OptionF0: "Show",
-        OptionF1: "Hide",
-        SetF0: "SourceCharacter makes DestinationCharacter AssetName reveal the front hair.",
-        SetF1: "SourceCharacter makes DestinationCharacter AssetName cover the front hair.",
-
-        Module隐藏后发: "Hide Back Hair",
-        Select隐藏后发: "Set Hide Back Hair",
-        OptionB0: "Show",
-        OptionB1: "Hide",
-        SetB0: "SourceCharacter makes DestinationCharacter AssetName reveal the back hair.",
-        SetB1: "SourceCharacter makes DestinationCharacter AssetName cover the back hair.",
-
-        Module隐藏其他: "Hide Other Accessories",
-        Select隐藏其他: "Set Hide Ears and Other Accessories",
-        OptionAC0: "Show",
-        OptionAC1: "Hide",
-        SetAC0: "SourceCharacter makes DestinationCharacter AssetName reveal the ears and other accessories.",
-        SetAC1: "SourceCharacter makes DestinationCharacter AssetName cover the ears and other accessories.",
-
-        Module图层: "Wearing Position",
-        Select图层: "Set Wearing Position",
-        OptionP0: "Above Head Items",
-        OptionP1: "Below Head Items",
-        SetP0: "SourceCharacter makes DestinationCharacter AssetName above head items",
-        SetP1: "SourceCharacter makes DestinationCharacter AssetName below head items",
-    },
-};
+    DialogTools.showHide({ moduleName: "隐藏前发", key: "F", moduleText: { CN: "前发", EN: "Front Hair" } }),
+    DialogTools.showHide({ moduleName: "隐藏后发", key: "B", moduleText: { CN: "后发", EN: "Back Hair" } }),
+    DialogTools.showHide({
+        moduleName: "隐藏其他",
+        key: "AC",
+        moduleText: { CN: "发饰和耳朵", EN: "Hair Acc. and Ears" },
+        fullText: { CN: "发饰和耳朵", EN: "Hair Accessories and Ears" },
+    })
+);
 
 const translation = {
     CN: "乳胶头套",
