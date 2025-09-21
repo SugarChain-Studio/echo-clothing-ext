51464bd5758c357810d6d20171b3b9755766f74d
dynilath <dynilath@qq.com>
2025-09-21 20:05:18 +0800

    feat: new layer mode for ghost body

--- src/components/衣服和装饰/替换身体/幽灵.js
+++ src/components/衣服和装饰/替换身体/幽灵.js
@@ -1,9 +1,26 @@
 import { AssetManager } from "../../../assetForward";
+import { Typing } from "../../../lib";
 import { adjustCanvasAlpha, partialDraw } from "./metaDraw";
-import { Tools } from "@mod-utils/Tools";
+import { ImageMapTools, PoseMapTools, Tools } from "@mod-utils/Tools";
+
+const mergingClothes = Typing.groups([
+    "Bra",
+    "Panties",
+    "Suit",
+    "SuitLower",
+    "Bra_笨笨蛋Luzi",
+    "Panties_笨笨蛋Luzi",
+    "Suit_笨笨蛋Luzi",
+    "SuitLower_笨笨蛋Luzi",
+    "Socks",
+    "SocksLeft",
+    "SocksRight",
+    "Corset",
+    "Garters",
+]);
 
 /** @type {(AssetLayerDefinition & {PGroups:CustomGroupName[], PGShow?:boolean})[]} */
-const bodyLayers = [
+const baseBodyLayers = [
     {
         Name: "HairBack",
         PGroups: ["HairBack", "新后发_Luzi"],
@@ -31,11 +48,13 @@ const bodyLayers = [
     {
         Name: "Body",
         PGroups: ["BodyUpper", "BodyLower", "ArmsLeft", "ArmsRight"],
+        AllowTypes: { h: [0, 1] },
         Priority: 7,
     },
     {
         Name: "Eyes",
         PGroups: ["Eyebrows", "Eyes", "右眼_Luzi", "Eyes2", "左眼_Luzi"],
+        AllowTypes: { h: [0, 1] },
         Priority: 9,
     },
     {
@@ -46,16 +65,19 @@ const bodyLayers = [
     {
         Name: "Mouth",
         PGroups: ["Mouth"],
+        AllowTypes: { h: [0, 1] },
         Priority: 10,
     },
     {
         Name: "Nipples",
         PGroups: ["Nipples"],
+        AllowTypes: { h: [0, 1] },
         Priority: 11,
     },
     {
         Name: "Pussy",
         PGroups: ["Pussy"],
+        AllowTypes: { h: [0, 1] },
         Priority: 12,
     },
     {
@@ -73,6 +95,28 @@ const bodyLayers = [
         Priority: 4,
     },
 ];
+
+const fullGroups = baseBodyLayers
+    .filter((l) => !l.PGShow)
+    .map((l) => l.PGroups)
+    .flat();
+
+const noHandsGroups = fullGroups.filter((g) => !["HandsLeft", "HandsRight"].includes(g));
+
+const bodyLayers = baseBodyLayers.concat([
+    {
+        Name: "FullBody",
+        PGroups: noHandsGroups,
+        AllowTypes: { h: 2 },
+        Priority: 12,
+    },
+    {
+        Name: "FullBody2",
+        PGroups: [...noHandsGroups, ...mergingClothes],
+        AllowTypes: { h: 3 },
+        Priority: 15,
+    },
+]);
 
 const bodyLayerNames = new Map(bodyLayers.map((layer) => [layer.Name, layer]));
 
@@ -100,14 +144,41 @@ function afterDraw(data, originalFunction, drawData) {
     const { C, A, X, Y, Color, drawCanvas, drawCanvasBlink, AlphaMasks, L, Property } = drawData;
     const layerDef = bodyLayerNames.get(L);
     if (layerDef && isBodyLayer(L)) {
-        const { Canvas, CanvasBlink } = partialDraw(C, A, layerDef.PGroups);
+        const pChara = { ...C, DrawAppearance: C.DrawAppearance.filter((a) => a.Asset.Name !== A.Name) };
+        const groups = (layerDef.PGroups || [])
+            .map((g) => C.Appearance.find((a) => a.Asset.Group.Name === g))
+            .filter(Boolean)
+            .filter((a) => CharacterAppearanceVisible(pChara, a.Asset.Name, a.Asset.Group.Name))
+            .map((a) => a.Asset.Group.Name);
+
+        if (groups.length === 0) return;
+
+        const { Canvas, CanvasBlink } = partialDraw(C, A, groups);
         const adjustedCanvas = adjustCanvasAlpha(C, A, Canvas, 0.5);
         const adjustedCanvasBlink = adjustCanvasAlpha(C, A, CanvasBlink, 0.5);
 
-        if (Property.TypeRecord?.f === 1 && L === "Body") {
-            const mask = Tools.getAssetURL(drawData, "Fade");
-            DrawImageEx(mask, adjustedCanvas.getContext("2d"), X, Y, { BlendingMode: "destination-out" });
-            DrawImageEx(mask, adjustedCanvasBlink.getContext("2d"), X, Y, { BlendingMode: "destination-out" });
+        if (L === "Body" || L === "FullBody" || L === "FullBody2") {
+            const ctx = adjustedCanvas.getContext("2d");
+            const ctxBlink = adjustedCanvasBlink.getContext("2d");
+            if (C.PoseMapping.BodyUpper === "BaseUpper" || C.PoseMapping.BodyUpper === "TapedHands") {
+                const bodySize = C.Appearance.find((a) => a.Asset.Group.Name === "BodyUpper");
+                if (bodySize) {
+                    for (const g of Typing.groups(["HandsLeft", "HandsRight"])) {
+                        const armMask = ImageMapTools.assetLayer(
+                            g,
+                            `${bodySize.Asset.Name}_Asian`,
+                            C.PoseMapping.BodyUpper === "TapedHands" ? "TapedHands" : undefined
+                        );
+                        DrawImageEx(armMask, ctx, X, Y, { BlendingMode: "destination-out" });
+                        DrawImageEx(armMask, ctxBlink, X, Y, { BlendingMode: "destination-out" });
+                    }
+                }
+            }
+            if (Property.TypeRecord?.f === 1) {
+                const mask = Tools.getAssetURL(drawData, "Fade");
+                DrawImageEx(mask, adjustedCanvas.getContext("2d"), X, Y, { BlendingMode: "destination-out" });
+                DrawImageEx(mask, adjustedCanvasBlink.getContext("2d"), X, Y, { BlendingMode: "destination-out" });
+            }
         }
 
         drawCanvas(adjustedCanvas, 0, 0, AlphaMasks);
@@ -139,12 +210,7 @@ const asset = {
     EditOpacity: true,
     DefaultColor: ["#DDDDDD", "#DDDDDD"],
     OverrideHeight: { Height: 30, Priority: 0 },
-    Hide: /**@type {CustomGroupBodyName[]}*/ (
-        bodyLayers
-            .filter((l) => !l.PGShow)
-            .map((layer) => layer.PGroups ?? [layer.Name])
-            .flat()
-    ),
+    Hide: fullGroups,
     Layer: [
         ...bodyLayers.map((l) => ({
             ...l,
@@ -161,6 +227,17 @@ const asset = {
             Name: "发光眼睛",
             HasImage: false,
             AllowTypes: { le: 1 },
+        },
+        {
+            Name: "HandMask",
+            Priority: 26,
+            ParentGroup: "BodyUpper",
+            TextureMask: {},
+            BlendingMode: "destination-out",
+            PoseMapping: PoseMapTools.FromTopHide({
+                BaseUpper: "",
+                TapedHands: "TapedHands",
+            }),
         },
     ],
 };
@@ -183,9 +260,9 @@ const extended = {
             Options: [{}, {}],
         },
         {
-            Name: "头发遮挡",
+            Name: "图层",
             Key: "h",
-            Options: [{}, {}],
+            Options: [{}, {}, {}, { Property: { Hide: mergingClothes } }],
         },
         {
             Name: "耳朵尾巴",
@@ -207,7 +284,6 @@ const assetStrings = {
         SelectBase: "选择幽灵身体设置",
         Module眼睛发光: "眼睛发光",
         Module脚渐隐: "脚渐隐",
-        Module头发遮挡: "头发图层",
         Module耳朵尾巴: "耳朵尾巴",
 
         Select眼睛发光: "选择眼睛发光设置",
@@ -218,9 +294,12 @@ const assetStrings = {
         Optionf0: "正常",
         Optionf1: "渐隐",
 
-        Select头发遮挡: "选择头发遮挡设置",
-        Optionh0: "显示头轮廓",
-        Optionh1: "结合前发图层",
+        Module图层: "图层",
+        Select图层: "选择图层合并设置",
+        Optionh0: "默认分离图层",
+        Optionh1: "合并头发和头",
+        Optionh2: "合并所有身体图层",
+        Optionh3: "合并身体和贴身衣服",
 
         Select耳朵尾巴: "选择耳朵尾巴设置",
         Optionet0: "实体化",
@@ -230,7 +309,6 @@ const assetStrings = {
         SelectBase: "Select Ghost Body Settings",
         Module眼睛发光: "Eye Glow",
         Module脚渐隐: "Foot Fade",
-        Module头发遮挡: "Hair Layering",
         Module耳朵尾巴: "Ear and Tail",
 
         Select眼睛发光: "Select Eye Glow Settings",
@@ -241,9 +319,12 @@ const assetStrings = {
         Optionf0: "Normal",
         Optionf1: "Fade",
 
-        Select头发遮挡: "Select Hair Mask Settings",
-        Optionh0: "Show Head Shape",
-        Optionh1: "Merge Front Hair",
+        Module图层: "Layering",
+        Select图层: "Select Layering Settings",
+        Optionh0: "Default Separate Layers",
+        Optionh1: "Merge Hair and Head",
+        Optionh2: "Merge All Body Layers",
+        Optionh3: "Merge Body and Underwear",
 
         Select耳朵尾巴: "Select Ear and Tail Settings",
         Optionet0: "Physical",
