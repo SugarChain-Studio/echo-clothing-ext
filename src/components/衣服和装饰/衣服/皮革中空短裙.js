ff2a6e3b0ae036621a827279d84194a0adadd8f2
dynilath <dynilath@qq.com>
2025-10-27 03:22:04 +0800

    feat: 皮革中空短裙调色能力增强

--- src/components/衣服和装饰/衣服/皮革中空短裙.js
+++ src/components/衣服和装饰/衣服/皮革中空短裙.js
@@ -1,42 +1,57 @@
 import { PoseMapTool } from "../../../lib";
 import { AssetManager } from "../../../assetForward";
 
-/** @type {CustomAssetDefinition} */
-const asset = {
-    Name: "皮革中空短裙",
-    Random: false,
-    Top: 0,
-    Left: {
-        [PoseType.DEFAULT]: 0,
-        KneelingSpread: 90,
+/** @type {AddAssetWithConfigParams} */
+const asset = [
+    "ClothLower",
+    {
+        Name: "皮革中空短裙",
+        Random: false,
+        Left: {
+            [PoseType.DEFAULT]: 120,
+            KneelingSpread: 210,
+        },
+        Top: 420,
+        Expose: ["ItemVulva", "ItemVulvaPiercings", "ItemButt"],
+        PoseMapping: PoseMapTool.hideFullBody(),
+        ParentGroup: {},
+        Priority: 26,
+        DefaultColor: ["Default", "Default", "#000000", "Default"],
+        Layer: [
+            { Name: "底", AllowTypes: { typed: 0 } },
+            { Name: "底_亮色", AllowTypes: { typed: [1, 2] } },
+            { Name: "底_暗色", AllowTypes: { typed: [1] } },
+            { Name: "反光" },
+        ],
     },
-    Expose: ["ItemVulva", "ItemVulvaPiercings", "ItemButt"],
-    PoseMapping: PoseMapTool.hideFullBody(),
-    ParentGroup: {},
-    Layer: [
-        {
-            Name: "底",
-            Priority: 26,
+    {
+        translation: { CN: "皮革中空短裙", EN: "Leather Sideway Skirt" },
+        layerNames: {
+            CN: { 底: "底色", 反光: "反光", 底_亮色: "分离/亮色底", 底_暗色: "暗色" },
+            EN: { 底: "Base", 反光: "Reflective", 底_亮色: "Separation/Light Base", 底_暗色: "Shade" },
         },
-        {
-            Name: "反光",
-            Priority: 26,
+        extended: {
+            Archetype: "typed",
+            DrawImages: false,
+            Options: [{ Name: "基础" }, { Name: "分离暗色" }, { Name: "浅色" }],
         },
-    ],
-};
-
-/** @type {Translation.Dialog} */
-const layerNames = {
-    CN: { 底: "底色", 反光: "反光" },
-    EN: { 底: "Base", 反光: "Reflective" },
-};
-
-/** @type {Translation.Entry} */
-const translation = {
-    CN: "皮革中空短裙",
-    EN: "Leather Sideway Skirt",
-};
+        assetStrings: {
+            CN: {
+                Select: "选择短裙图层样式",
+                基础: "基础",
+                分离暗色: "分离暗色",
+                浅色: "浅色",
+            },
+            EN: {
+                Select: "Select skirt layer style",
+                基础: "Basic",
+                分离暗色: "Dark Separation",
+                浅色: "Light Color",
+            },
+        },
+    },
+];
 
 export default function () {
-    AssetManager.addAssetWithConfig("ClothLower", asset, { translation, layerNames });
+    AssetManager.addAssetWithConfig(...asset);
 }
