d7c44b487f6b739305f7ea27a41d9be306916da7
dynilath <dynilath@qq.com>
2025-11-18 02:54:29 +0800

    refactor: 正确加载Fixups

--- src/lib/fixups.js
+++ src/lib/fixups.js
@@ -1,4 +1,5 @@
 import { HookManager } from "@sugarch/bc-mod-hook-manager";
+import { AssetManager } from "../assetForward";
 
 /**
  * @type {(typeof LoginInventoryFixups)}
@@ -12,9 +13,29 @@ export function customFixup(fixup) {
     myFixups.push(/** @type {typeof myFixups[number]} */ (fixup));
 }
 
-export default function () {
-    HookManager.hookFunction("LoginPerformInventoryFixups", 0, (args, next) => {
-        LoginInventoryFixups.push(...myFixups);
-        return next(args);
-    });
+/** @param {PlayerCharacter} player */
+function performWardrobeFixup(player) {
+    /** @type {Map<string, CustomLoginInventoryFixup["New"]>} */
+    const oldNewMap = new Map(myFixups.map((fixup) => [`${fixup.Old.Group}::${fixup.Old.Name}`, fixup.New]));
+
+    for (const dressup of player.Wardrobe) {
+        for (const dress of dressup) {
+            const key = `${dress.Group}::${dress.Name}`;
+            if (oldNewMap.has(key)) {
+                const newInfo = oldNewMap.get(key);
+                dress.Group = /** @type {AssetGroupName}*/ (newInfo.Group);
+                dress.Name = newInfo.Name;
+            }
+        }
+    }
 }
+
+AssetManager.afterLoad(() => {
+    LoginInventoryFixups.push(...myFixups);
+});
+
+HookManager.hookFunction("LoginPerformCraftingFixups", 0, (args, next) => {
+    performWardrobeFixup(Player);
+
+    return next(args);
+});
