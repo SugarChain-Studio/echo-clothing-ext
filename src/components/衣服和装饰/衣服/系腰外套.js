a5869d864970f62c99d34a0c3632f010254cc5f9
Echo <1260556316@qq.com>
2026-01-21 01:29:13 +0800

    feat：`系腰外套`

---
+++ src/components/衣服和装饰/衣服/系腰外套.js
@@ -0,0 +1,55 @@
+import { AssetManager } from "../../../assetForward";
+import { luziSuffixFixups } from "../../../lib/fixups";
+
+/** @type { AddAssetWithConfigParams[] }} */
+const assets = [
+    [
+        ["ClothOuter", "ClothLower", "Cloth"],
+        {
+            Name: "系腰外套小",
+            Random: false,
+            Top: 400,
+            Left: 153,
+            DynamicGroupName: "ClothOuter",
+            ParentGroup: {},
+            PoseMapping: {},
+            Layer: [
+                { Name: "A1", Priority: 5 },
+                { Name: "A2" },
+            ],
+        },
+        { translation: { CN: "系腰外套小", EN: "Small Waist-Tied Outerwear" } },
+    ],
+];
+
+/** @type { AddAssetWithConfigParams[] }} */
+const assets2 = [
+    [
+        ["ClothOuter", "ClothLower", "Cloth"],
+        {
+            Name: "系腰外套大",
+            Random: false,
+            Top: 400,
+            Left: 140,
+            DynamicGroupName: "ClothOuter",
+            ParentGroup: {},
+            PoseMapping: {},
+            Layer: [
+                { Name: "A1", Priority: 5 },
+                { Name: "A2" },
+            ],
+        },
+        { translation: { CN: "系腰外套大", EN: "Large Waist-Tied Outerwear" } },
+    ],
+];
+
+export default function () {
+    AssetManager.addAssetWithConfig(assets);
+    for (const asset of assets) {
+        luziSuffixFixups(asset[0], asset[1].Name);
+    }
+    AssetManager.addAssetWithConfig(assets2);
+    for (const asset of assets2) {
+        luziSuffixFixups(asset[0], asset[1].Name);
+    }
+}
