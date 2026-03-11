1629d3fcc3808d925e071c80f4d3b8e61c9c7822
dynilath <dynilath@qq.com>
2026-03-12 01:23:55 +0800

    refactor: 优化天线实现，修改名称，调整图层名翻译

--- src/components/衣服和装饰/饰品/天线.js
+++ src/components/衣服和装饰/饰品/天线.js
@@ -1,81 +1,102 @@
 import { AssetManager } from "../../../../src/assetForward";
+import { Typing } from "../../../lib";
 
-/** @type { CustomAssetDefinition } */
-const asset = {
-    Name: "天线",
-    Random: false,
-    Top: 0,
-    Left: 0,
-    ParentGroup: {},
-    DefaultColor: ["#8F8F8F", "#000000", "#131313", "#5C0000", "#600000", "#8F8F8F", "#000000", "#131313", "#5C0000", "#600000"],
-    Layer: [
-        { Name: "A1", Priority: 55, AllowTypes: [{ typed: 0 }, { typed: 1 }, { typed: 4 }, { typed: 5 }] },
-        { Name: "A2", Priority: 55, AllowTypes: [{ typed: 0 }, { typed: 1 }, { typed: 4 }, { typed: 5 }] },
-        { Name: "A3", Priority: 55, AllowTypes: [{ typed: 0 }, { typed: 1 }, { typed: 4 }, { typed: 5 }] },
-        { Name: "A4", Priority: 55, AllowTypes: [{ typed: 0 }, { typed: 1 }, { typed: 4 }, { typed: 5 }] },
-        { Name: "A5", Priority: 55, AllowTypes: [{ typed: 1 }, { typed: 5 }] },
-
-        { Name: "B1", Priority: 55, AllowTypes: [{ typed: 2 }, { typed: 3 }, { typed: 4 }, { typed: 5 }] },
-        { Name: "B2", Priority: 55, AllowTypes: [{ typed: 2 }, { typed: 3 }, { typed: 4 }, { typed: 5 }] },
-        { Name: "B3", Priority: 55, AllowTypes: [{ typed: 2 }, { typed: 3 }, { typed: 4 }, { typed: 5 }] },
-        { Name: "B4", Priority: 55, AllowTypes: [{ typed: 2 }, { typed: 3 }, { typed: 4 }, { typed: 5 }] },
-        { Name: "B5", Priority: 55, AllowTypes: [{ typed: 3 }, { typed: 5 }] },
-    ],
-};
-
-const layerNames = {
-    EN: {
-        A1: "A1",
-        A2: "A2",
-        A3: "A3",
-        A4: "A4",
-        A5: "A5",
+/** @type { AddAssetWithConfigParams } */
+const asset = [
+    ["HairAccessory1", "HairAccessory3"],
+    {
+        Name: "天线",
+        Random: false,
+        Left: 170,
+        Top: 10,
+        ParentGroup: {},
+        DefaultColor: [
+            "#8F8F8F",
+            "#000000",
+            "#131313",
+            "#5C0000",
+            "#600000",
+            "#8F8F8F",
+            "#000000",
+            "#131313",
+            "#5C0000",
+            "#600000",
+        ],
+        Priority: 55,
+        Layer: [
+            ...Typing.layerMap(
+                [
+                    { Name: "A1", ColorGroup: "主体" },
+                    { Name: "A2", ColorGroup: "侧面" },
+                    { Name: "A3", ColorGroup: "天线" },
+                    { Name: "A4", ColorGroup: "灯" },
+                ],
+                (l) => ({ AllowTypes: [{ typed: 0 }, { typed: 1 }, { typed: 4 }, { typed: 5 }], ...l })
+            ),
+            { Name: "A5", AllowTypes: [{ typed: 1 }, { typed: 5 }], ColorGroup: "发光" },
+            ...Typing.layerMap(
+                [
+                    { Name: "B1", ColorGroup: "主体" },
+                    { Name: "B2", ColorGroup: "侧面" },
+                    { Name: "B3", ColorGroup: "天线" },
+                    { Name: "B4", ColorGroup: "灯" },
+                ],
+                (l) => ({ AllowTypes: [{ typed: 2 }, { typed: 3 }, { typed: 4 }, { typed: 5 }], ...l })
+            ),
+            { Name: "B5", AllowTypes: [{ typed: 3 }, { typed: 5 }], ColorGroup: "发光" },
+        ],
     },
-};
-
-/** @type {AssetArchetypeConfig} */
-const extended = {
-    Archetype: ExtendedArchetype.TYPED,
-    DrawImages: false,
-    Options: [
-        { Name: "右" },
-        { Name: "右发光" },
-        { Name: "左" },
-        { Name: "左发光" },
-        { Name: "左右" },
-        { Name: "左右发光" },
-    ],
-};
-
-/** @type {Translation.Dialog} */
-const assetStrings = {
-    CN: {
-        Select: "选择",
-        右: "右",
-        右发光: "右发光",
-        左: "左",
-        左发光: "左发光",
-        左右: "左右",
-        左右发光: "左右发光",
-
+    {
+        translation: { CN: "天线耳机", EN: "Antenna Headphone" },
+        layerNames: {
+            CN: {
+                ...Typing.repeatEntries([["A1", "A2", "A3", "A4"], "右"]),
+                ...Typing.repeatEntries([["B1", "B2", "B3", "B4"], "左"]),
+            },
+            EN: {
+                ...Typing.repeatEntries([["A1", "A2", "A3", "A4"], "Right"]),
+                ...Typing.repeatEntries([["B1", "B2", "B3", "B4"], "Left"]),
+                主体: "Main",
+                侧面: "Side",
+                天线: "Antenna",
+                灯: "Light",
+            },
+        },
+        extended: {
+            Archetype: ExtendedArchetype.TYPED,
+            DrawImages: false,
+            Options: [
+                { Name: "右" },
+                { Name: "右发光" },
+                { Name: "左" },
+                { Name: "左发光" },
+                { Name: "左右" },
+                { Name: "左右发光" },
+            ],
+        },
+        assetStrings: {
+            CN: {
+                Select: "选择",
+                右: "右",
+                右发光: "右发光",
+                左: "左",
+                左发光: "左发光",
+                左右: "左右",
+                左右发光: "左右发光",
+            },
+            EN: {
+                Select: "Select Pattern",
+                右: "Right",
+                右发光: "Right Glowing",
+                左: "Left",
+                左发光: "Left Glowing",
+                左右: "Left Right",
+                左右发光: "Left Right Glowing",
+            },
+        },
     },
-    EN: {
-        Select: "Select Pattern",
-        右: "Right",
-        右发光: "Right Glowing",
-        左: "Left",
-        左发光: "Left Glowing",
-        左右: "Left Right",
-        左右发光: "Left Right Glowing",
-    },
-};
-
-const translation = {
-    CN: "天线",
-    EN: "Antenna",
-};
+];
 
 export default function () {
-    AssetManager.addAssetWithConfig("HairAccessory1", asset, { translation, layerNames, extended, assetStrings });
-    AssetManager.addAssetWithConfig("HairAccessory3", asset, { translation, layerNames, extended, assetStrings });
+    AssetManager.addAssetWithConfig(...asset);
 }
