a90eafc47561e6c9bd10da9ccf1e0eb8fd93c90b
Echo <1260556316@qq.com>
2026-07-28 10:36:55 +0800

    chore: 玩偶

--- src/components/衣服和装饰/饰品/手持物/玩偶.js
+++ src/components/衣服和装饰/饰品/手持物/玩偶.js
@@ -593,6 +593,7 @@ const plushieItems = [
     { name: "樱", roomAbbr: "qq" },
     { name: "Penny", roomAbbr: "qq" },
     { name: "樱和七分白衣", roomAbbr: "qq" },
+    { name: "蓝月", roomAbbr: "qq" },
 
     // 观察者之庭
     { name: "妄羽", roomAbbr: "gcz" },
@@ -681,6 +682,8 @@ const plushieItems = [
     { name: "柳晓", roomAbbr: "l" },
     { name: "妄羽", roomAbbr: "l" },
     { name: "羽和柚", roomAbbr: "l" },
+    { name: "RuKa", roomAbbr: "l" },
+    { name: "白月薇", roomAbbr: "l" },
 ];
 
 // ========== 生成函数 ==========
@@ -846,7 +849,7 @@ const optionCount = asset.Layer.reduce((pv, cv) => {
     const Key = Object.keys(cv.AllowTypes)[0];
     Access.set(pv, Key, Math.max(Access.getOr(pv, Key, 0), Access.getOr(cv.AllowTypes, Key, 0)));
     return pv;
-}, /** @type { Record<string, Number> } */ ({}));
+}, /** @type { Record<string, Number> } */({}));
 
 /**
  * 生成模块定义
@@ -871,7 +874,7 @@ const typedLayerNames = /** @type {AssetLayerDefinition[]}*/ (asset.Layer).reduc
     const idx = Access.getOr(cv.AllowTypes, k, 0);
     pv[k][idx] = cv.Name;
     return pv;
-}, /** @type { Record<keyof typeof typeNameNext, Record<number,string>> } */ ({}));
+}, /** @type { Record<keyof typeof typeNameNext, Record<number,string>> } */({}));
 
 /** 注册菜单模块 */
 modules.forEach((m) => {
@@ -912,7 +915,7 @@ const layerNames = /** @type {AssetLayerDefinition[]}*/ (asset.Layer).reduce((pv
     const [k, v] = Object.entries(cv.AllowTypes)[0];
     pv[`${takeShortName(typeNameNext[k], "CN")}${v}`] = cv.Name;
     return pv;
-}, /** @type { Record<string,string> } */ ({}));
+}, /** @type { Record<string,string> } */({}));
 
 const cnDialog = DialogTools.dialogGenerator(
     modules,
